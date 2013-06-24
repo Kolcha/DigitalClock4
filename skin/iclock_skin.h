4612562d4d72f9b28dc067299fc01153ce8d0373
Nick Korotysh <nick.korotysh@gmail.com>
2013-06-24 13:22:46 +0300

    changed skin info structure

--- skin/iclock_skin.h
+++ skin/iclock_skin.h
@@ -5,12 +5,24 @@
 #include <QString>
 #include <QPixmap>
 
+// skin info fields
+enum FSkinInfo {
+  SI_NAME,        // skin name
+  SI_VERSION,     // skin version
+  SI_AUTHOR,      // skin author
+  SI_EMAIL,       // author's e-mail
+  SI_COMMENT,     // skin description
+  SI_TYPE         // skin type ('bitmap/vector')
+};
+// skin info type
+typedef QMap<FSkinInfo, QString> TSkinInfo;
+
 class IClockSkin {
 public:
   virtual ~IClockSkin() {}
 
-  virtual const QMap<QString, QString>& GetSkinInfo() const = 0;
-  virtual QPixmap* GetImage(QChar ch, qreal zoom, bool cache = true) = 0;
+  virtual const TSkinInfo& GetInfo() const = 0;
+  virtual QPixmap* GetImage(const QString& s, qreal zoom, bool cache) = 0;
 };
 
 #endif // ICLOCK_SKIN_H
