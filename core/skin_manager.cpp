d3c06a47845460c9209aef8b4f7fbe48d92fd775
Nick Korotysh <nick.korotysh@gmail.com>
2013-06-26 10:36:53 +0300

    implemented skin manager
    --HG--
    branch : skin_manager

--- core/skin_manager.cpp
+++ core/skin_manager.cpp
@@ -1,5 +1,35 @@
+#include <QDir>
+#include "../skin/iclock_skin.h"
 #include "skin_manager.h"
 
 SkinManager::SkinManager(QObject *parent)
   : QObject(parent) {
+}
+
+void SkinManager::AddSkinDir(const QString& dir) {
+  skin_dirs_.append(dir);
+}
+
+void SkinManager::DelSkinDir(const QString& dir) {
+  skin_dirs_.removeOne(dir);
+}
+
+void SkinManager::ListSkins() {
+  for (auto& s_dir : skin_dirs_) {
+    QDir d(s_dir);
+    QStringList f_dirs = d.entryList(QStringList("*"),
+                                     QDir::NoDotAndDotDot | QDir::AllDirs);
+    for (auto& f_dir : f_dirs) {
+      QString skin_root = QDir(s_dir).filePath(f_dir);
+      IClockSkin* tmp = CreateSkin(skin_root);
+      if (!tmp) continue;
+      skins_[tmp->GetInfo()[SI_NAME]] = skin_root;
+      delete tmp;
+    }
+  }
+  emit SearchFinished(skins_.keys());
+}
+
+void SkinManager::FindSkin(const QString& skin_name) {
+  emit SkinFound(skins_[skin_name]);
 }
--- core/skin_manager.h
+++ core/skin_manager.h
@@ -1,7 +1,9 @@
 #ifndef SKIN_MANAGER_H
 #define SKIN_MANAGER_H
 
-#include <QObject>
+#include <QMap>
+#include <QString>
+#include <QStringList>
 
 class SkinManager : public QObject {
   Q_OBJECT
@@ -9,9 +11,18 @@ public:
   explicit SkinManager(QObject *parent = 0);
 
 signals:
+  void SearchFinished(const QStringList& skins);
+  void SkinFound(const QString& skin_root);
 
 public slots:
+  void AddSkinDir(const QString& dir);
+  void DelSkinDir(const QString& dir);
+  void ListSkins();
+  void FindSkin(const QString& skin_name);
 
+private:
+  QStringList skin_dirs_;
+  QMap<QString, QString> skins_;
 };
 
 #endif // SKIN_MANAGER_H
