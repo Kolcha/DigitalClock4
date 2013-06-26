92881f6f6eb8742fd3a7a65d00be2770bdc4da81
Nick Korotysh <nick.korotysh@gmail.com>
2013-06-26 11:41:12 +0300

    added new files
    --HG--
    branch : clock_settings

---
+++ core/clock_settings.cpp
@@ -0,0 +1,5 @@
+#include "clock_settings.h"
+
+ClockSettings::ClockSettings(QObject *parent)
+  : QObject(parent) {
+}
---
+++ core/clock_settings.h
@@ -0,0 +1,17 @@
+#ifndef CLOCK_SETTINGS_H
+#define CLOCK_SETTINGS_H
+
+#include <QObject>
+
+class ClockSettings : public QObject {
+  Q_OBJECT
+public:
+  explicit ClockSettings(QObject *parent = 0);
+
+signals:
+
+public slots:
+
+};
+
+#endif // CLOCK_SETTINGS_H
