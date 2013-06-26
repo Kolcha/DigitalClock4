55f4a110eed051be440a1d4dd88407244e9d47c7
Nick Korotysh <nick.korotysh@gmail.com>
2013-06-26 10:46:00 +0300

    fixed skin factory
    --HG--
    branch : skin_manager

--- skin/skin_factory.cpp
+++ skin/skin_factory.cpp
@@ -5,9 +5,10 @@
 IClockSkin* CreateSkin(const QString& skin_root) {
   QDir root(skin_root);
   QStringList images = root.entryList(QStringList("*.svg"), QDir::Files);
-  if (images.empty()) {
-    return new RasterSkin(skin_root);
-  } else {
-    return new VectorSkin(skin_root);
-  }
+
+  IClockSkin* skin = 0;
+  if (!images.empty()) skin = new VectorSkin(skin_root);
+  images = root.entryList(QStringList("*.png"), QDir::Files);
+  if (!images.empty()) skin = new RasterSkin(skin_root);
+  return skin;
 }
