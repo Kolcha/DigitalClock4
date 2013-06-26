b3b134e3380005695f020b3ce94db8c5d3905862
Nick Korotysh <nick.korotysh@gmail.com>
2013-06-26 13:01:46 +0300

    added application info
    --HG--
    branch : clock_settings

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include "widget.h"
 #include <QApplication>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
+    a.setApplicationDisplayName("Digital Clock 4");
+    a.setApplicationName("Digital Clock 4");
+    a.setApplicationVersion("4.1.4");
+    a.setOrganizationName("Nick Korotysh");
     Widget w;
     w.show();
-    
+
     return a.exec();
 }
