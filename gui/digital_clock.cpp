52e0348467ede27f6f17887afc5bab474bda75ab
Nick Korotysh <nick.korotysh@gmail.com>
2013-06-26 14:15:25 +0300

    implemented get + draw time logic
    --HG--
    branch : gui_main

--- gui/digital_clock.cpp
+++ gui/digital_clock.cpp
@@ -1,10 +1,34 @@
 #include <QHBoxLayout>
+#include <QTime>
 #include "digital_clock.h"
 
 DigitalClock::DigitalClock(QWidget *parent)
   : QWidget(parent) {
-  clock_ = new QLabel();
+  display_ = new QLabel();
   QHBoxLayout* main_layout = new QHBoxLayout();
-  main_layout->addWidget(clock_);
+  main_layout->addWidget(display_);
   setLayout(main_layout);
+
+  timer_.setTimerType(Qt::PreciseTimer);
+  timer_.setInterval(500);
+  connect(&timer_, SIGNAL(timeout()), this, SLOT(TimeoutHandler()));
+  timer_.start();
+
+  sep_state_ = false;
+}
+
+DigitalClock::~DigitalClock() {
+  timer_.stop();
+}
+
+void DigitalClock::DrawImage(const QImage& image) {
+  display_->setPixmap(QPixmap::fromImage(image));
+}
+
+void DigitalClock::TimeoutHandler() {
+  QString time = QTime::currentTime().toString("hh:mm");
+  time[2] = sep_state_ ? ':' : ' ';
+  sep_state_ = !sep_state_;
+  emit ImageNeeded(time);
+  setWindowTitle(time);
 }
--- gui/digital_clock.h
+++ gui/digital_clock.h
@@ -3,18 +3,27 @@
 
 #include <QWidget>
 #include <QLabel>
+#include <QTimer>
 
 class DigitalClock : public QWidget {
   Q_OBJECT
 public:
   explicit DigitalClock(QWidget *parent = 0);
+  ~DigitalClock();
 
 signals:
+  void ImageNeeded(const QString& text);
 
 public slots:
+  void DrawImage(const QImage& image);
+
+private slots:
+  void TimeoutHandler();
 
 private:
-  QLabel* clock_;
+  QLabel* display_;
+  QTimer timer_;
+  bool sep_state_;
 };
 
 #endif // DIGITAL_CLOCK_H
