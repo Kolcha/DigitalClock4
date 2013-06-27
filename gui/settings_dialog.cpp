fb60fd5f1d7d06c44d847945cbcd3e326da5445d
Nick Korotysh <nick.korotysh@gmail.com>
2013-06-27 11:26:35 +0300

    added new files
    --HG--
    branch : settings_dlg

---
+++ gui/settings_dialog.cpp
@@ -0,0 +1,26 @@
+#include "settings_dialog.h"
+#include "ui_settings_dialog.h"
+
+SettingsDialog::SettingsDialog(QWidget *parent) :
+  QDialog(parent),
+  ui(new Ui::SettingsDialog)
+{
+  ui->setupUi(this);
+}
+
+SettingsDialog::~SettingsDialog()
+{
+  delete ui;
+}
+
+void SettingsDialog::changeEvent(QEvent *e)
+{
+  QDialog::changeEvent(e);
+  switch (e->type()) {
+    case QEvent::LanguageChange:
+      ui->retranslateUi(this);
+      break;
+    default:
+      break;
+  }
+}
