b73dede78cf936257b38870880a082fbc382d9b9
Nick Korotysh <nick.korotysh@gmail.com>
2013-07-03 01:59:47 +0300

    dialog creation/deletion
    --HG--
    branch : about_dlg

--- gui/about_dialog.cpp
+++ gui/about_dialog.cpp
@@ -1,14 +1,13 @@
 #include "about_dialog.h"
 #include "ui_about_dialog.h"
 
-AboutDialog::AboutDialog(QWidget *parent) :
-  QDialog(parent),
-  ui(new Ui::AboutDialog)
-{
+AboutDialog::AboutDialog(QWidget *parent)
+  : QDialog(parent), ui(new Ui::AboutDialog) {
   ui->setupUi(this);
+  setAttribute(Qt::WA_DeleteOnClose);
+  setWindowIcon(QIcon(":/images/about.svg"));
 }
 
-AboutDialog::~AboutDialog()
-{
+AboutDialog::~AboutDialog() {
   delete ui;
 }
--- gui/about_dialog.h
+++ gui/about_dialog.h
@@ -7,14 +7,13 @@ namespace Ui {
 class AboutDialog;
 }
 
-class AboutDialog : public QDialog
-{
+class AboutDialog : public QDialog {
   Q_OBJECT
-  
+
 public:
   explicit AboutDialog(QWidget *parent = 0);
   ~AboutDialog();
-  
+
 private:
   Ui::AboutDialog *ui;
 };
