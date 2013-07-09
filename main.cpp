fece1b2d51765ea5adad667d47f7380d1b6e44c7
Nick Korotysh <nick.korotysh@gmail.com>
2013-07-09 09:19:59 +0300

    changed app name

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 int main(int argc, char *argv[]) {
   QApplication a(argc, argv);
   a.setApplicationDisplayName("Digital Clock");
-  a.setApplicationName("Digital Clock 4");
+  a.setApplicationName("Digital Clock");
   a.setApplicationVersion("4.1.4");
   a.setOrganizationName("Nick Korotysh");
   a.setWindowIcon(QIcon(":/images/about_qt.svg"));
