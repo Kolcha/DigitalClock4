e35fcb740c62a8b2926ea0370d5b615a626d027f
Nick Korotysh <nick.korotysh@gmail.com>
2013-07-13 02:42:30 +0300

    update translations
    --HG--
    branch : new_about_dlg

--- languages/digital_clock_ru.ts
+++ languages/digital_clock_ru.ts
@@ -4,211 +4,273 @@
 <context>
     <name>AboutDialog</name>
     <message>
-        <location filename="gui/about_dialog.ui" line="14"/>
+        <location filename="../gui/about_dialog.ui" line="14"/>
         <source>About</source>
         <translatorcomment>заголовок диалога о программе</translatorcomment>
         <translation>О программе</translation>
     </message>
     <message>
-        <location filename="gui/about_dialog.ui" line="25"/>
-        <location filename="gui/about_dialog.ui" line="54"/>
-        <location filename="gui/about_dialog.ui" line="68"/>
-        <location filename="gui/about_dialog.ui" line="82"/>
-        <location filename="gui/about_dialog.ui" line="96"/>
-        <source>TextLabel</source>
-        <translatorcomment>значение</translatorcomment>
-        <translation>значение</translation>
+        <location filename="../gui/about_dialog.ui" line="32"/>
+        <source>program name</source>
+        <translation></translation>
     </message>
     <message>
-        <location filename="gui/about_dialog.ui" line="47"/>
-        <source>name:</source>
-        <translatorcomment>имя программы</translatorcomment>
-        <translation>название :</translation>
+        <location filename="../gui/about_dialog.ui" line="44"/>
+        <source>copyright</source>
+        <translation></translation>
     </message>
     <message>
-        <location filename="gui/about_dialog.ui" line="61"/>
-        <source>version:</source>
-        <translatorcomment>версия программы</translatorcomment>
-        <translation>версия :</translation>
+        <location filename="../gui/about_dialog.ui" line="56"/>
+        <source>version</source>
+        <translation></translation>
     </message>
     <message>
-        <location filename="gui/about_dialog.ui" line="75"/>
-        <source>Qt version:</source>
-        <translatorcomment>вирсия используемой библиотеки Qt</translatorcomment>
-        <translation>версия Qt :</translation>
+        <location filename="../gui/about_dialog.ui" line="90"/>
+        <source>Links</source>
+        <translatorcomment>ссылки на интернет ресурсы</translatorcomment>
+        <translation>Ссылки</translation>
     </message>
     <message>
-        <location filename="gui/about_dialog.ui" line="89"/>
-        <source>Copyright:</source>
-        <translatorcomment>информация о разработчике</translatorcomment>
-        <translation>Copyright :</translation>
+        <location filename="../gui/about_dialog.ui" line="96"/>
+        <source>information:</source>
+        <translatorcomment>информация о прорамме и ссылки на загрузку</translatorcomment>
+        <translation>информация:</translation>
     </message>
     <message>
-        <location filename="gui/about_dialog.ui" line="125"/>
+        <location filename="../gui/about_dialog.ui" line="103"/>
+        <source>&lt;a href=&quot;http://www.softpedia.com/get/Desktop-Enhancements/Clocks-Time-Management/Korotysh-Digital-Clock.shtml&quot;&gt;www.softpedia.com&lt;/a&gt;</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../gui/about_dialog.ui" line="116"/>
+        <source>source code:</source>
+        <translatorcomment>ссылки на исходный код</translatorcomment>
+        <translation>исходный код:</translation>
+    </message>
+    <message>
+        <location filename="../gui/about_dialog.ui" line="123"/>
+        <source>&lt;a href=&quot;http://sourceforge.net/projects/digitalclock4/&quot;&gt;sourceforge.net&lt;/a&gt;</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../gui/about_dialog.ui" line="136"/>
+        <source>e-mail:</source>
+        <translatorcomment>e-mail автора</translatorcomment>
+        <translation>e-mail:</translation>
+    </message>
+    <message>
+        <location filename="../gui/about_dialog.ui" line="143"/>
+        <source>&lt;a href=&quot;mailto:nick.korotysh@gmail.com&quot;&gt;nick.korotysh@gmail.com&lt;/a&gt;</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../gui/about_dialog.ui" line="159"/>
+        <source>Donate</source>
+        <translatorcomment>информация для пожертвований</translatorcomment>
+        <translation>Помочь автору</translation>
+    </message>
+    <message>
+        <location filename="../gui/about_dialog.ui" line="165"/>
+        <source>WMZ:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../gui/about_dialog.ui" line="185"/>
+        <source>Z275611204281</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../gui/about_dialog.ui" line="195"/>
+        <source>WME:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../gui/about_dialog.ui" line="215"/>
+        <source>E241249077263</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../gui/about_dialog.ui" line="225"/>
+        <source>WMR:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../gui/about_dialog.ui" line="245"/>
+        <source>R263617824302</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../gui/about_dialog.ui" line="275"/>
         <source>Close</source>
         <translatorcomment>Закрыть окно</translatorcomment>
         <translation>Закрыть</translation>
+    </message>
+    <message>
+        <location filename="../gui/about_dialog.cpp" line="12"/>
+        <source>version: </source>
+        <translatorcomment>версия программы</translatorcomment>
+        <translation>версия: </translation>
     </message>
 </context>
 <context>
     <name>SettingsDialog</name>
     <message>
-        <location filename="gui/settings_dialog.ui" line="14"/>
+        <location filename="../gui/settings_dialog.ui" line="14"/>
         <source>Settings</source>
         <translatorcomment>настройки программы</translatorcomment>
         <translation>Настройки</translation>
     </message>
     <message>
-        <location filename="gui/settings_dialog.ui" line="20"/>
+        <location filename="../gui/settings_dialog.ui" line="20"/>
         <source>Clock</source>
         <translatorcomment>группа настроек поведения часов</translatorcomment>
         <translation>Часы</translation>
     </message>
     <message>
-        <location filename="gui/settings_dialog.ui" line="28"/>
+        <location filename="../gui/settings_dialog.ui" line="28"/>
         <source>stay on top</source>
         <translatorcomment>часы остаются поверх всех окон</translatorcomment>
         <translation>поверх всех окон</translation>
     </message>
     <message>
-        <location filename="gui/settings_dialog.ui" line="35"/>
+        <location filename="../gui/settings_dialog.ui" line="35"/>
         <source>transparent for input</source>
         <translatorcomment>часы програзчны для кликов мыши</translatorcomment>
         <translation>прозрачность для мыши</translation>
     </message>
     <message>
-        <location filename="gui/settings_dialog.ui" line="42"/>
+        <location filename="../gui/settings_dialog.ui" line="42"/>
         <source>separator flashes</source>
         <translatorcomment>разделитель часов и минут мигает</translatorcomment>
         <translation>разделитель мигает</translation>
     </message>
     <message>
-        <location filename="gui/settings_dialog.ui" line="69"/>
+        <location filename="../gui/settings_dialog.ui" line="69"/>
         <source>opacity</source>
         <translatorcomment>непрозрачность часов</translatorcomment>
         <translation>непрозрачность</translation>
     </message>
     <message>
-        <location filename="gui/settings_dialog.ui" line="95"/>
-        <location filename="gui/settings_dialog.ui" line="132"/>
+        <location filename="../gui/settings_dialog.ui" line="95"/>
+        <location filename="../gui/settings_dialog.ui" line="132"/>
         <source>100</source>
         <translatorcomment>100</translatorcomment>
         <translation>100</translation>
     </message>
     <message>
-        <location filename="gui/settings_dialog.ui" line="102"/>
-        <location filename="gui/settings_dialog.ui" line="139"/>
+        <location filename="../gui/settings_dialog.ui" line="102"/>
+        <location filename="../gui/settings_dialog.ui" line="139"/>
         <source>%</source>
         <translatorcomment>знак %</translatorcomment>
         <translation>%</translation>
     </message>
     <message>
-        <location filename="gui/settings_dialog.ui" line="109"/>
+        <location filename="../gui/settings_dialog.ui" line="109"/>
         <source>zoom</source>
         <translatorcomment>масштаб</translatorcomment>
         <translation>увеличение</translation>
     </message>
     <message>
-        <location filename="gui/settings_dialog.ui" line="151"/>
+        <location filename="../gui/settings_dialog.ui" line="151"/>
         <source>Skin</source>
         <translatorcomment>стиль часов</translatorcomment>
         <translation>Стиль</translation>
     </message>
     <message>
-        <location filename="gui/settings_dialog.ui" line="160"/>
+        <location filename="../gui/settings_dialog.ui" line="160"/>
         <source>&lt;no comment&gt;</source>
         <translatorcomment>значение по умолчанию для сведений</translatorcomment>
         <translation>&lt;нет сведений&gt;</translation>
     </message>
     <message>
-        <location filename="gui/settings_dialog.ui" line="170"/>
+        <location filename="../gui/settings_dialog.ui" line="170"/>
         <source>Version :</source>
         <translatorcomment>версия стиля</translatorcomment>
         <translation>Версия :</translation>
     </message>
     <message>
-        <location filename="gui/settings_dialog.ui" line="177"/>
-        <location filename="gui/settings_dialog.ui" line="191"/>
-        <location filename="gui/settings_dialog.ui" line="205"/>
+        <location filename="../gui/settings_dialog.ui" line="177"/>
+        <location filename="../gui/settings_dialog.ui" line="191"/>
+        <location filename="../gui/settings_dialog.ui" line="205"/>
         <source>unknown</source>
         <translatorcomment>значение по умолчанию</translatorcomment>
         <translation>неизвестно</translation>
     </message>
     <message>
-        <location filename="gui/settings_dialog.ui" line="184"/>
+        <location filename="../gui/settings_dialog.ui" line="184"/>
         <source>Author :</source>
         <translatorcomment>автор стиля</translatorcomment>
         <translation>Автор :</translation>
     </message>
     <message>
-        <location filename="gui/settings_dialog.ui" line="198"/>
+        <location filename="../gui/settings_dialog.ui" line="198"/>
         <source>E-mail :</source>
         <translatorcomment>эелектронный адрес автора стиля</translatorcomment>
         <translation>E-mail :</translation>
     </message>
     <message>
-        <location filename="gui/settings_dialog.ui" line="215"/>
+        <location filename="../gui/settings_dialog.ui" line="215"/>
         <source>Texture</source>
         <translatorcomment>группа настроек тектуры</translatorcomment>
         <translation>Текстура</translation>
     </message>
     <message>
-        <location filename="gui/settings_dialog.ui" line="221"/>
+        <location filename="../gui/settings_dialog.ui" line="221"/>
         <source>texture per element</source>
         <translatorcomment>использовать текстуру для каждого элемента (цифры) отдельно</translatorcomment>
         <translation>текстура для каждого элемента</translation>
     </message>
     <message>
-        <location filename="gui/settings_dialog.ui" line="228"/>
+        <location filename="../gui/settings_dialog.ui" line="228"/>
         <source>Type</source>
         <translatorcomment>тип текстуры</translatorcomment>
         <translation>Тип</translation>
     </message>
     <message>
-        <location filename="gui/settings_dialog.ui" line="234"/>
+        <location filename="../gui/settings_dialog.ui" line="234"/>
         <source>solid color</source>
         <translatorcomment>сплошной цвет как текстура</translatorcomment>
         <translation>сплошной цвет</translation>
     </message>
     <message>
-        <location filename="gui/settings_dialog.ui" line="241"/>
+        <location filename="../gui/settings_dialog.ui" line="241"/>
         <source>custom image</source>
         <translatorcomment>использовать картинку как текстуру</translatorcomment>
         <translation>изображение</translation>
     </message>
     <message>
-        <location filename="gui/settings_dialog.ui" line="257"/>
-        <location filename="gui/settings_dialog.ui" line="273"/>
+        <location filename="../gui/settings_dialog.ui" line="257"/>
+        <location filename="../gui/settings_dialog.ui" line="273"/>
         <source>...</source>
         <translatorcomment>...</translatorcomment>
         <translation>...</translation>
     </message>
     <message>
-        <location filename="gui/settings_dialog.ui" line="283"/>
+        <location filename="../gui/settings_dialog.ui" line="283"/>
         <source>Mode</source>
         <translatorcomment>режим отрисовки текстуры</translatorcomment>
         <translation>Режим</translation>
     </message>
     <message>
-        <location filename="gui/settings_dialog.ui" line="289"/>
+        <location filename="../gui/settings_dialog.ui" line="289"/>
         <source>stretch</source>
         <translatorcomment>растянуть текстуру</translatorcomment>
         <translation>растянуть</translation>
     </message>
     <message>
-        <location filename="gui/settings_dialog.ui" line="296"/>
+        <location filename="../gui/settings_dialog.ui" line="296"/>
         <source>tile</source>
         <translatorcomment>замостить текстуру</translatorcomment>
         <translation>замостить</translation>
     </message>
     <message>
-        <location filename="gui/settings_dialog.cpp" line="158"/>
+        <location filename="../gui/settings_dialog.cpp" line="159"/>
         <source>Open texture file</source>
         <translatorcomment>найти и выбрать файл текстуры</translatorcomment>
         <translation>Открыть файл текстуры</translation>
     </message>
     <message>
-        <location filename="gui/settings_dialog.cpp" line="160"/>
+        <location filename="../gui/settings_dialog.cpp" line="161"/>
         <source>Images (*.bmp *.jpg *.jpeg *.png *.tiff *.xbm *.xpm)</source>
         <translatorcomment>форматы картинок</translatorcomment>
         <translation>Картинки (*.bmp *.jpg *.jpeg *.png *.tiff *.xbm *.xpm)</translation>
@@ -217,25 +279,25 @@
 <context>
     <name>TrayControl</name>
     <message>
-        <location filename="gui/tray_control.cpp" line="15"/>
+        <location filename="../gui/tray_control.cpp" line="15"/>
         <source>&amp;Settings</source>
         <translatorcomment>настройки программы</translatorcomment>
         <translation>&amp;Настройки</translation>
     </message>
     <message>
-        <location filename="gui/tray_control.cpp" line="18"/>
+        <location filename="../gui/tray_control.cpp" line="18"/>
         <source>&amp;About</source>
         <translatorcomment>информация о программе</translatorcomment>
         <translation>&amp;О программе</translation>
     </message>
     <message>
-        <location filename="gui/tray_control.cpp" line="21"/>
+        <location filename="../gui/tray_control.cpp" line="21"/>
         <source>About &amp;Qt</source>
         <translatorcomment>информация о библиотеке Qt</translatorcomment>
         <translation>О библиотеке &amp;Qt</translation>
     </message>
     <message>
-        <location filename="gui/tray_control.cpp" line="24"/>
+        <location filename="../gui/tray_control.cpp" line="24"/>
         <source>&amp;Quit</source>
         <translatorcomment>выход из программы</translatorcomment>
         <translation>&amp;Выход</translation>
