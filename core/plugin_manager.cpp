6cd49b56d8805579ea97f1b5536ba0446eae6d3c
Nick Korotysh <nick.korotysh@gmail.com>
2013-07-14 20:55:36 +0300

    implemented plugin manager
    --HG--
    branch : plugin_manager

--- core/plugin_manager.cpp
+++ core/plugin_manager.cpp
@@ -3,3 +3,57 @@
 PluginManager::PluginManager(QObject *parent)
   : QObject(parent) {
 }
+
+void PluginManager::SetInitData(const TPluginData& data) {
+  data_ = data;
+}
+
+void PluginManager::AddPluginsDir(const QDir& dir) {
+  plugins_dirs_.append(dir);
+}
+
+void PluginManager::DelPluginsDir(const QDir& dir) {
+  plugins_dirs_.removeOne(dir);
+}
+
+void PluginManager::ListAvailable() {
+  available_.clear();
+  for (auto& dir : plugins_dirs_) {
+    QStringList files = dir.entryList(QDir::Files);
+    for (auto& file : files) {
+      available_[file] = dir.filePath(file);
+    }
+  }
+  emit SearchFinished(available_.keys());
+}
+
+void PluginManager::LoadPlugins(const QStringList& files) {
+  for (auto& file : files) {
+    LoadPlugin(file);
+  }
+}
+
+void PluginManager::EnablePlugin(const QString& name, bool enable) {
+  QString file = available_[name];
+  enable ? LoadPlugin(file) : UnloadPlugin(file);
+}
+
+void PluginManager::LoadPlugin(const QString& file) {
+  QPluginLoader* loader = new QPluginLoader(file, this);
+  IClockPlugin* plugin = qobject_cast<IClockPlugin*>(loader->instance());
+  if (plugin) {
+    plugin->Init(data_);
+    plugin->Start();
+    loaded_[file] = loader;
+  }
+}
+
+void PluginManager::UnloadPlugin(const QString& file) {
+  QPluginLoader* loader = loaded_[file];
+  IClockPlugin* plugin = qobject_cast<IClockPlugin*>(loader->instance());
+  if (plugin) {
+    plugin->Stop();
+    loader->unload();
+    loaded_.remove(file);
+  }
+}
--- core/plugin_manager.h
+++ core/plugin_manager.h
@@ -1,17 +1,37 @@
 #ifndef PLUGIN_MANAGER_H
 #define PLUGIN_MANAGER_H
 
-#include <QObject>
+#include <QMap>
+#include <QDir>
+#include <QString>
+#include <QStringList>
+#include <QPluginLoader>
+#include "iclock_plugin.h"
 
 class PluginManager : public QObject {
   Q_OBJECT
 public:
   explicit PluginManager(QObject *parent = 0);
+  void SetInitData(const TPluginData& data);
 
 signals:
+  void SearchFinished(const QStringList& skins);
 
 public slots:
+  void AddPluginsDir(const QDir& dir);
+  void DelPluginsDir(const QDir& dir);
+  void ListAvailable();
+  void LoadPlugins(const QStringList& files);
+  void EnablePlugin(const QString& name, bool enable);
 
+private:
+  void LoadPlugin(const QString& file);
+  void UnloadPlugin(const QString& file);
+
+  QList<QDir> plugins_dirs_;
+  QMap<QString, QString> available_;
+  QMap<QString, QPluginLoader*> loaded_;
+  TPluginData data_;
 };
 
 #endif // PLUGIN_MANAGER_H
