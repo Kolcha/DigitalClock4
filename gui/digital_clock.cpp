8baff2795c3660c128eb8e74db103cc75b7e521e
Nick Korotysh <nick.korotysh@gmail.com>
2013-07-14 14:01:11 +0300

    added time format management
    --HG--
    branch : api_for_plugins

--- gui/digital_clock.cpp
+++ gui/digital_clock.cpp
@@ -27,8 +27,13 @@ void DigitalClock::SetSeparatorFlash(bool set) {
   sep_visible_ = !set;
 }
 
+void DigitalClock::SetTimeFormat(const QString& format) {
+  time_format_ = format;
+}
+
 void DigitalClock::TimeoutHandler() {
-  QString time = QLocale::system().toString(QTime::currentTime());
+  if (time_format_.isEmpty()) time_format_ = QLocale::system().timeFormat();
+  QString time = QTime::currentTime().toString(time_format_);
   int sep_pos = time.indexOf(':');
   time = time.mid(0, time.lastIndexOf(':'));
   if (sep_flashes_) {
