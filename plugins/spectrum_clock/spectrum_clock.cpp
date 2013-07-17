c185064a45fef19fef04e32dee82435b039f50c1
Nick Korotysh <nick.korotysh@gmail.com>
2013-07-17 21:32:45 +0300

    spectrum clock plugin implemented
    --HG--
    branch : plugins

--- plugins/spectrum_clock/spectrum_clock.cpp
+++ plugins/spectrum_clock/spectrum_clock.cpp
@@ -1,5 +1,50 @@
+#include "core/clock_settings.h"
+#include "gui/digital_clock.h"
 #include "spectrum_clock.h"
 
-SpectrumClock::SpectrumClock(QObject *parent)
-  : QDecorationPlugin(parent) {
+SpectrumClock::SpectrumClock() {
+  info_[PI_NAME] = "Spectrum Clock";
+  info_[PI_VERSION] = "2.0";
+  info_[PI_AUTHOR] = "Nick Korotysh";
+  info_[PI_EMAIL] = "nick.korotysh@gmail.com";
+  info_[PI_COMMENT] = "clock changes color";
+}
+
+void SpectrumClock::Init(const TPluginData& data) {
+  data_ = data;
+  cur_color_ = Qt::red;
+}
+
+void SpectrumClock::Config() {
+}
+
+void SpectrumClock::Start() {
+  old_color_ = data_.settings->GetOption(OPT_COLOR).value<QColor>();
+  connect(data_.clock, SIGNAL(ImageNeeded(QString)), this, SLOT(TimeoutHandler(QString)));
+}
+
+void SpectrumClock::Stop() {
+  disconnect(data_.clock, SIGNAL(ImageNeeded(QString)), this, SLOT(TimeoutHandler(QString)));
+  data_.settings->SetOption(OPT_COLOR, old_color_);
+}
+
+const TPluginInfo&SpectrumClock::GetInfo() const {
+  return info_;
+}
+
+void SpectrumClock::TimeoutHandler(const QString&) {
+  int r = cur_color_.red();
+  int g = cur_color_.green();
+  int b = cur_color_.blue();
+  int i = 1;
+
+  if (r == 255 && g  < 255 && b ==  0 ) { g += i; }
+  if (r  >  0  && g == 255 && b ==  0 ) { r -= i; }
+  if (r ==  0  && g == 255 && b  < 255) { b += i; }
+  if (r ==  0  && g  >  0  && b == 255) { g -= i; }
+  if (r  < 255 && g ==  0  && b == 255) { r += i; }
+  if (r == 255 && g ==  0  && b  >  0 ) { b -= i; }
+
+  cur_color_.setRgb(r, g, b);
+  data_.settings->SetOption(OPT_COLOR, cur_color_);
 }
--- plugins/spectrum_clock/spectrum_clock.h
+++ plugins/spectrum_clock/spectrum_clock.h
@@ -1,13 +1,30 @@
 #ifndef SPECTRUM_CLOCK_H
 #define SPECTRUM_CLOCK_H
 
+#include <QColor>
 #include "core/iclock_plugin.h"
 
 class SpectrumClock : public QObject, public IClockPlugin {
   Q_OBJECT
+  Q_PLUGIN_METADATA(IID PLUGIN_INTERFACE_IID FILE "spectrum_clock.json")
+  Q_INTERFACES(IClockPlugin)
 
 public:
-  SpectrumClock(QObject *parent = 0);
+  SpectrumClock();
+  void Init(const TPluginData& data);
+  void Config();
+  void Start();
+  void Stop();
+  const TPluginInfo& GetInfo() const;
+
+private slots:
+  void TimeoutHandler(const QString&);
+
+private:
+  TPluginInfo info_;
+  TPluginData data_;
+  QColor old_color_;
+  QColor cur_color_;
 };
 
 #endif // SPECTRUM_CLOCK_H
