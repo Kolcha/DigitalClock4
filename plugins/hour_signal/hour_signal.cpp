8a1d2b7eba241e0a2915b62f858813bd5e51aed7
Nick Korotysh <nick.korotysh@gmail.com>
2013-07-30 21:31:00 +0300

    implemented GetInfo
    --HG--
    branch : plugins

--- plugins/hour_signal/hour_signal.cpp
+++ plugins/hour_signal/hour_signal.cpp
@@ -1,6 +1,12 @@
 #include "hour_signal.h"
 
 void HourSignal::GetInfo(TPluginInfo* info) {
+  info->insert(PI_NAME, "Every hour signal");
+  info->insert(PI_TYPE, "clock");
+  info->insert(PI_VERSION, "2.0");
+  info->insert(PI_AUTHOR, "Nick Korotysh");
+  info->insert(PI_EMAIL, "nick.korotysh@gmail.com");
+  info->insert(PI_COMMENT, "Plays short signal every hour.");
 }
 
 void HourSignal::TimeUpdateListener(const QString& current_time) {
