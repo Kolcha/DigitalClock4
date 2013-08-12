6a15d60bd91096817b7d71924e82316a3039bf4b
Nick Korotysh <nick.korotysh@gmail.com>
2013-08-12 08:24:27 +0300

    implemented plugin list widget item
    --HG--
    branch : plugins_gui

--- gui/plugin_list_item.cpp
+++ gui/plugin_list_item.cpp
@@ -1,17 +1,63 @@
-#include <QCheckBox>
-#include <QPushButton>
 #include <QHBoxLayout>
 #include "plugin_list_item.h"
 
+PluginListItem::PluginListItem(QWidget* parent)
+  : QWidget(parent) {
+  Init();
+}
+
 PluginListItem::PluginListItem(const QString& text, bool enabled, bool congurable, QWidget* parent)
   : QWidget(parent) {
-  QCheckBox* chk = new QCheckBox(this);
-  QPushButton* btn = new QPushButton("Config", this);
-  btn->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
+  Init();
+  SetText(text);
+  SetChecked(enabled);
+  SetConfigurable(congurable);
+}
+
+QString PluginListItem::GetText() const {
+  return chk_->text();
+}
+
+bool PluginListItem::IsChecked() const {
+  return chk_->isChecked();
+}
+
+bool PluginListItem::IsConfigurable() const {
+  return btn_->isEnabled();
+}
+
+void PluginListItem::SetText(const QString& text) {
+  chk_->setText(text);
+}
+
+void PluginListItem::SetChecked(bool checked) {
+  chk_->setChecked(checked);
+}
+
+void PluginListItem::SetConfigurable(bool configurable) {
+  btn_->setEnabled(configurable);
+}
+
+void PluginListItem::CheckBoxToggled(bool checked) {
+  emit StateChanged(chk_->text(), checked);
+}
+
+void PluginListItem::BtnClicked() {
+  emit ConfigureRequest(chk_->text());
+}
+
+void PluginListItem::Init() {
+  chk_ = new QCheckBox(this);
+  btn_ = new QToolButton(this);
+  btn_->setIcon(QIcon(":/images/settings.svg"));
+  btn_->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
+
   QHBoxLayout* hlt = new QHBoxLayout(this);
-  hlt->addWidget(chk);
-  hlt->addWidget(btn);
+  hlt->addWidget(chk_);
+  hlt->addWidget(btn_);
+  hlt->setMargin(2);
   setLayout(hlt);
-  connect(chk, SIGNAL(toggled(bool)), this, SIGNAL(StateChanged(bool)));
-  connect(btn, SIGNAL(clicked()), this, SIGNAL(ConfigureRequest()));
+
+  connect(chk_, SIGNAL(toggled(bool)), this, SLOT(CheckBoxToggled(bool)));
+  connect(btn_, SIGNAL(clicked()), this, SLOT(BtnClicked()));
 }
--- gui/plugin_list_item.h
+++ gui/plugin_list_item.h
@@ -1,7 +1,8 @@
 #ifndef PLUGIN_LIST_ITEM_H
 #define PLUGIN_LIST_ITEM_H
 
-#include <QWidget>
+#include <QCheckBox>
+#include <QToolButton>
 
 /*! @brief Plugins list item widget.
  *
@@ -15,6 +16,11 @@ class PluginListItem : public QWidget {
 public:
   /*!
    * Constructor.
+   * @param parent - parent widget
+   */
+  explicit PluginListItem(QWidget* parent = 0);
+  /*!
+   * Constructor.
    * @param text - text to display (plugin name)
    * @param enabled - plugin state (true means enabled)
    * @param congurable - is plugin configurable
@@ -22,16 +28,70 @@ public:
    */
   PluginListItem(const QString& text, bool enabled, bool congurable, QWidget* parent = 0);
 
+  /*!
+   * Get item text.
+   * @return item text
+   */
+  QString GetText() const;
+  /*!
+   * Get item checked state.
+   * @return item checked state
+   */
+  bool IsChecked() const;
+  /*!
+   * Ask is item have settings button.
+   * @return true if settings button is enabled
+   */
+  bool IsConfigurable() const;
+
+public slots:
+  /*!
+   * Set item text
+   * @param text - text to set
+   */
+  void SetText(const QString& text);
+  /*!
+   * Set item checked state
+   * @param checked - new state
+   */
+  void SetChecked(bool checked);
+  /*!
+   * Set configurable property.
+   * @param configurable - true to enable
+   */
+  void SetConfigurable(bool configurable);
+
 signals:
   /*!
    * Signal to notify that user changed checkbox state.
+   * @param text - current item text
    * @param new_state - new checkbox state
    */
-  void StateChanged(bool new_state);
+  void StateChanged(const QString& text, bool new_state);
   /*!
    * This signal is emited when user click on settings button.
+   * @param text - current item text
    */
-  void ConfigureRequest();
+  void ConfigureRequest(const QString& text);
+
+private slots:
+  /*!
+   * Slot to handle checkbox toggled signal.
+   * @param checked - new checked state
+   */
+  void CheckBoxToggled(bool checked);
+  /*!
+   * Slot to handle button clicked signal.
+   */
+  void BtnClicked();
+
+private:
+  /*!
+   * Init widget.
+   */
+  void Init();
+  QCheckBox* chk_;
+  QToolButton* btn_;
 };
 
 #endif // PLUGIN_LIST_ITEM_H
