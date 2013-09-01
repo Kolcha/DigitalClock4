feb3d8da369750ed08730ca52f4ae02dc7c43beb
Nick Korotysh <nick.korotysh@gmail.com>
2013-09-01 09:54:43 +0300

    implemented settings dialog
    --HG--
    branch : plugins

--- plugins/alarm/gui/settings_dlg.cpp
+++ plugins/alarm/gui/settings_dlg.cpp
@@ -1,3 +1,5 @@
+#include <QFileInfo>
+#include <QFileDialog>
 #include "../alarm_settings.h"
 #include "settings_dlg.h"
 #include "ui_settings_dlg.h"
@@ -24,6 +26,7 @@ void SettingsDlg::SettingsListener(const QString& key, const QVariant& value) {
     QString file = value.toString();
     ui->default_signal->setChecked(file == DEFAULT_SIGNAL);
     ui->custom_signal->setChecked(file != DEFAULT_SIGNAL);
+    last_file_path_ = (file == DEFAULT_SIGNAL) ? "." : QFileInfo(file).absolutePath();
   }
   if (key == OPT_SHOW_NOTIFY) {
     ui->notification_enabled->setChecked(value.toBool());
@@ -31,4 +34,34 @@ void SettingsDlg::SettingsListener(const QString& key, const QVariant& value) {
   if (key == OPT_NOTIFY_TEXT) {
     ui->message_edit->setPlainText(value.toString());
   }
+}
+
+void SettingsDlg::on_time_edit_timeChanged(const QTime& time) {
+  emit OptionChanged(OPT_TIME, time);
+}
+
+void SettingsDlg::on_alarm_enabled_toggled(bool checked) {
+  emit OptionChanged(OPT_ENABLED, checked);
+}
+
+void SettingsDlg::on_browse_btn_clicked() {
+  QString sound_file = QFileDialog::getOpenFileName(this, tr("Select sound file"),
+                       last_file_path_,
+                       tr("Sounds (*.wav *.mp3)"));
+  if (!sound_file.isEmpty()) {
+    emit OptionChanged(OPT_SIGNAL, sound_file);
+    last_file_path_ = QFileInfo(sound_file).absolutePath();
+  }
+}
+
+void SettingsDlg::on_notification_enabled_toggled(bool checked) {
+  emit OptionChanged(OPT_SHOW_NOTIFY, checked);
+}
+
+void SettingsDlg::on_message_edit_textChanged() {
+  emit OptionChanged(OPT_NOTIFY_TEXT, ui->message_edit->toPlainText());
+}
+
+void SettingsDlg::on_default_signal_clicked() {
+  emit OptionChanged(OPT_SIGNAL, DEFAULT_SIGNAL);
 }
--- plugins/alarm/gui/settings_dlg.h
+++ plugins/alarm/gui/settings_dlg.h
@@ -20,8 +20,17 @@ public slots:
 signals:
   void OptionChanged(const QString&, const QVariant&);
 
+private slots:
+  void on_time_edit_timeChanged(const QTime& time);
+  void on_alarm_enabled_toggled(bool checked);
+  void on_browse_btn_clicked();
+  void on_notification_enabled_toggled(bool checked);
+  void on_message_edit_textChanged();
+  void on_default_signal_clicked();
+
 private:
   Ui::SettingsDlg* ui;
+  QString last_file_path_;
 };
 
 #endif // SETTINGS_DLG_H
