7b31a837bf4f212b6ac75ffe9d49964c3927fe3d
Nick Korotysh <nick.korotysh@gmail.com>
2013-10-25 23:50:36 +0300

    changed ICLockSkin
    --HG--
    branch : new_draw_engine

--- skin_draw/iclock_skin.h
+++ skin_draw/iclock_skin.h
@@ -1,41 +1,49 @@
 #ifndef ICLOCK_SKIN_H
 #define ICLOCK_SKIN_H
 
-#include "skin_draw_global.h"
-
-#include <QDir>
 #include <QMap>
-#include <QFont>
 #include <QString>
 #include <QPixmap>
 #include <QSharedPointer>
 
-// TODO: add comments
-// skin info fields
-enum FSkinInfo {
-  SI_NAME,        // skin name
-  SI_VERSION,     // skin version
-  SI_AUTHOR,      // skin author
-  SI_EMAIL,       // author's e-mail
-  SI_COMMENT      // skin description
-};
-// skin info type
-typedef QMap<FSkinInfo, QString> TSkinInfo;
-
-typedef QSharedPointer<QPixmap> QPixmapPtr;
-
+/*!
+ * @brief The IClockSkin class.
+ *
+ * Clock skin interface.
+ */
 class IClockSkin {
 public:
+  /*! Virtual destructor. */
   virtual ~IClockSkin() {}
 
-  virtual void GetInfo(TSkinInfo* info) = 0;
+  /*! skin info fields */
+  enum FSkinInfo {
+    SI_NAME,        /*!< skin name */
+    SI_VERSION,     /*!< skin version */
+    SI_AUTHOR,      /*!< skin author */
+    SI_EMAIL,       /*!< author's e-mail */
+    SI_COMMENT      /*!< skin description */
+  };
+  /// skin info type
+  typedef QMap<FSkinInfo, QString> TSkinInfo;
+  /// clock skin pointer type
+  typedef QSharedPointer<IClockSkin> ClockSkinPtr;
+  /// QPixmap shared pointer type
+  typedef QSharedPointer<QPixmap> QPixmapPtr;
+
+  /*!
+   * Get skin info.
+   * @return skin info
+   */
+  virtual const TSkinInfo& GetInfo(TSkinInfo* info) const = 0;
+  /*!
+   * Get image for specified symbol.
+   * @param ch - symbol for which need to get image
+   * @param zoom - skin zoom
+   * @param cache - is image add to cache
+   * @return pointer to result image
+   */
   virtual QPixmapPtr GetImage(QChar ch, qreal zoom, bool cache) = 0;
 };
-
-typedef QSharedPointer<IClockSkin> ClockSkinPtr;
-
-// TODO: move other file
-SKIN_DRAWSHARED_EXPORT ClockSkinPtr CreateSkin(const QDir& skin_root);
-SKIN_DRAWSHARED_EXPORT ClockSkinPtr CreateSkin(const QFont& font);
 
 #endif // ICLOCK_SKIN_H
