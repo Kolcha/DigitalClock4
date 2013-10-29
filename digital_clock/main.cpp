67e199a870cb55685fe3a7ef066d321373452bb1
Nick Korotysh <nick.korotysh@gmail.com>
2013-10-29 09:17:00 +0300

    update version
    --HG--
    branch : new_installer

--- digital_clock/main.cpp
+++ digital_clock/main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char *argv[]) {
   QApplication a(argc, argv);
   a.setApplicationDisplayName("Digital Clock");
   a.setApplicationName("Digital Clock");
-  a.setApplicationVersion("4.2.2");
+  a.setApplicationVersion("4.2.3");
   a.setOrganizationName("Nick Korotysh");
   a.setWindowIcon(QIcon(":/images/clock.svg"));
 
