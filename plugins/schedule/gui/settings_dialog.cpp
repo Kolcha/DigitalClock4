c7ed6a9d4c366c4b19a36ddc0e3975e48eb3ebba
Nick Korotysh <nick.korotysh@gmail.com>
2013-11-13 10:57:45 +0300

    implemented tasks dialog
    --HG--
    branch : plugins

--- plugins/schedule/gui/settings_dialog.cpp
+++ plugins/schedule/gui/settings_dialog.cpp
@@ -11,8 +11,41 @@ SettingsDialog::~SettingsDialog() {
   delete ui;
 }
 
+void SettingsDialog::SetDates(const QList<QDate>& dates) {
+  for (auto& date : dates) {
+    ui->days_box->addItem(date.toString(Qt::SystemLocaleShortDate), date);
+  }
+  ui->days_box->setCurrentText(QDate::currentDate().toString(Qt::SystemLocaleShortDate));
+}
+
+void SettingsDialog::SetTasks(const QMap<QTime, QString>& tasks) {
+  ui->tasks_table->clear();
+  for (auto iter = tasks.begin(); iter != tasks.end(); ++iter) {
+    int index = ui->tasks_table->rowCount();
+    ui->tasks_table->setRowCount(index + 1);
+    QTableWidgetItem* item1 = new QTableWidgetItem(iter.key().toString(Qt::SystemLocaleShortDate));
+    item1->setData(Qt::UserRole, iter.key());
+    QTableWidgetItem* item2 = new QTableWidgetItem(iter.value());
+    ui->tasks_table->setItem(index, 0, item1);
+    ui->tasks_table->setItem(index - 1, 1, item2);
+  }
+}
+
+void SettingsDialog::on_days_box_currentIndexChanged(int index) {
+  emit DateChanged(ui->days_box->itemData(index).value<QDate>());
+}
+
 void SettingsDialog::on_add_btn_clicked() {
   AddTaskDialog* dlg = new AddTaskDialog(this);
   connect(dlg, SIGNAL(TaskAdded(Task)), this, SIGNAL(TaskAdded(Task)));
   dlg->show();
+}
+
+void SettingsDialog::on_del_btn_clicked() {
+  Task task;
+  task.date = ui->days_box->itemData(ui->days_box->currentIndex()).value<QDate>();
+  int row = ui->tasks_table->currentRow();
+  task.time = ui->tasks_table->item(row, 0)->data(Qt::UserRole).value<QTime>();
+  task.description = ui->tasks_table->item(row, 1)->text();
+  emit TaskRemoved(task);
 }
--- plugins/schedule/gui/settings_dialog.h
+++ plugins/schedule/gui/settings_dialog.h
@@ -18,9 +18,16 @@ public:
 signals:
   void TaskAdded(const Task& task);
   void TaskRemoved(const Task& task);
+  void DateChanged(const QDate& new_date);
+
+public slots:
+  void SetDates(const QList<QDate>& dates);
+  void SetTasks(const QMap<QTime, QString>& tasks);
 
 private slots:
+  void on_days_box_currentIndexChanged(int index);
   void on_add_btn_clicked();
+  void on_del_btn_clicked();
 
 private:
   Ui::SettingsDialog* ui;
