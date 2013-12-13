a6889ddb1db9d2ce0d60cf7f91fed5baf830bd63
Nick Korotysh <nick.korotysh@gmail.com>
2013-12-13 03:36:25 +0300

    implemented updater class
    --HG--
    branch : updater

--- digital_clock/core/updater.cpp
+++ digital_clock/core/updater.cpp
@@ -1,12 +1,42 @@
+#include <QJsonDocument>
+#include <QJsonObject>
 #include "updater.h"
 
 Updater::Updater(QObject* parent)
-  : QObject(parent), check_beta_(false) {
+  : QObject(parent),
+    downloader_("digitalclock4.sourceforge.net", "/latest.json"),
+    check_beta_(false) {
+  connect(&downloader_, &HttpClient::ErrorMessage, this, &Updater::ErrorMessage);
+  connect(&downloader_, &HttpClient::DataDownloaded,
+          [=] (const QByteArray& data) { data_.append(data); });
+  connect(&downloader_, &HttpClient::finished, this, &Updater::ProcessData);
+}
+
+Updater::~Updater() {
+  if (downloader_.isRunning()) downloader_.wait();
 }
 
 void Updater::CheckForUpdates() {
+  downloader_.start();
 }
 
 void Updater::SetCheckForBeta(bool check) {
   check_beta_ = check;
+}
+
+void Updater::ProcessData() {
+  QJsonParseError err;
+  QJsonDocument js_doc = QJsonDocument::fromJson(data_, &err);
+  if (err.error != QJsonParseError::NoError) {
+    emit ErrorMessage(err.errorString());
+    return;
+  }
+  QJsonObject js_obj = js_doc.object().value("stable").toObject();
+  QString latest = js_obj.value("version").toString();
+  if (check_beta_) {
+    js_obj = js_doc.object().value("testing").toObject();
+    QString t_version = js_obj.value("version").toString("-");
+    latest = t_version == "-" ? latest : t_version;
+  }
+  emit LatestVersion(latest);
 }
--- digital_clock/core/updater.h
+++ digital_clock/core/updater.h
@@ -1,22 +1,29 @@
 #ifndef UPDATER_H
 #define UPDATER_H
 
-#include <QObject>
+#include "http_client.h"
 
 class Updater : public QObject {
   Q_OBJECT
 public:
   explicit Updater(QObject* parent = 0);
+  ~Updater();
 
 signals:
-  void UpdateAvailable(const QString& new_version);
+  void LatestVersion(const QString& new_version);
+  void ErrorMessage(const QString& msg);
 
 public slots:
   void CheckForUpdates();
   void SetCheckForBeta(bool check);
 
+private slots:
+  void ProcessData();
+
 private:
+  HttpClient downloader_;
   bool check_beta_;
+  QByteArray data_;
 };
 
 #endif // UPDATER_H
