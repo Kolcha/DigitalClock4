dd909f4ccf2b5b46135e7088c245d2dabe947f78
Nick Korotysh <nick.korotysh@gmail.com>
2013-12-21 19:26:53 +0300

    fixed translation issue in app
    --HG--
    branch : improve_plugins_engine

--- digital_clock/skin/clock_vector_skin.cpp
+++ digital_clock/skin/clock_vector_skin.cpp
@@ -4,11 +4,11 @@
 ClockVectorSkin::ClockVectorSkin(const QDir& skin_root) {
   QSettings config(skin_root.filePath("skin.ini"), QSettings::IniFormat);
   // load info
-  info_[SI_NAME] = config.value("info/name", "unknown").toString();
-  info_[SI_VERSION] = config.value("info/version", "unknown").toString();
-  info_[SI_AUTHOR] = config.value("info/author", "unknown").toString();
-  info_[SI_EMAIL] = config.value("info/e-mail", "unknown").toString();
-  info_[SI_COMMENT] = config.value("info/comment", "unknown").toString();
+  info_[SI_NAME] = config.value("info/name").toString();
+  info_[SI_VERSION] = config.value("info/version").toString();
+  info_[SI_AUTHOR] = config.value("info/author").toString();
+  info_[SI_EMAIL] = config.value("info/e-mail").toString();
+  info_[SI_COMMENT] = config.value("info/comment").toString();
   // load image files
   for (int i = 0; i < 10; ++i) {
     QString key = QString::number(i);
