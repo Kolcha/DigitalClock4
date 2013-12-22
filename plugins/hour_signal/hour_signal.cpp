4f56ded62ff2fa024d2ad90a3a748cb0f2fcb575
Nick Korotysh <nick.korotysh@gmail.com>
2013-12-23 00:34:00 +0300

    hour_signal plugin in own namespace
    --HG--
    branch : code_cleanup

--- plugins/hour_signal/hour_signal.cpp
+++ plugins/hour_signal/hour_signal.cpp
@@ -2,8 +2,12 @@
 #include <QSound>
 #include "hour_signal.h"
 
+namespace hour_signal {
+
 void HourSignal::TimeUpdateListener() {
   int current_hour = QTime::currentTime().toString("H").toInt();
-  if (current_hour != last_hour_ && last_hour_ != -1) QSound::play(":/hour_signal.wav");
+  if (current_hour != last_hour_ && last_hour_ != -1) QSound::play(":/hour_signal/hour_signal.wav");
   last_hour_ = current_hour;
 }
+
+} // namespace hour_signal
