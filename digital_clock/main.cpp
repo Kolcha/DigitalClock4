dbbf38eb764b038be461f02234114b163f201f78
Nick Korotysh <nick.korotysh@gmail.com>
2013-12-27 07:39:28 +0300

    fixed system language autodetect
    --HG--
    branch : plugin_info_translation

--- digital_clock/main.cpp
+++ digital_clock/main.cpp
@@ -7,19 +7,32 @@ int main(int argc, char *argv[]) {
   QApplication a(argc, argv);
   a.setApplicationDisplayName("Digital Clock");
   a.setApplicationName("Digital Clock");
-  a.setApplicationVersion("4.2.5");
+  a.setApplicationVersion("4.2.5+");
   a.setOrganizationName("Nick Korotysh");
   a.setWindowIcon(QIcon(":/clock/images/clock.svg"));
   a.setQuitOnLastWindowClosed(false);
 
-  QTranslator qt_translator;
-  qt_translator.load("qt_" + QLocale::system().name(),
-                     QLibraryInfo::location(QLibraryInfo::TranslationsPath));
-  a.installTranslator(&qt_translator);
-
   QTranslator app_translator;
-  app_translator.load(":/clock/languages/digital_clock_" + QLocale::system().name());
-  a.installTranslator(&app_translator);
+  QTranslator qt_translator;
+  QStringList ui_languages = QLocale::system().uiLanguages();
+  foreach (QString locale, ui_languages) {
+    locale = QLocale(locale).name();
+    if (app_translator.load(QLatin1String(":/clock/languages/digital_clock_") + locale)) {
+      if (qt_translator.load(QLatin1String("qt_") + locale,
+                             QLibraryInfo::location(QLibraryInfo::TranslationsPath))) {
+        a.installTranslator(&app_translator);
+        a.installTranslator(&qt_translator);
+        break;
+      }
+      app_translator.load(QString()); // unload()
+    } else if (locale == QLatin1String("C") /* overrideLanguage == "English" */) {
+      // use built-in
+      break;
+    } else if (locale.startsWith(QLatin1String("en")) /* "English" is built-in */) {
+      // use built-in
+      break;
+    }
+  }
 
   digital_clock::MainWindow w;
   w.Init();
