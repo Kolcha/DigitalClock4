587364b5212f6925d664cbd54b224267b69a2d27
Nick Korotysh <nick.korotysh@gmail.com>
2013-12-31 00:14:33 +0300

    implemented draw logic
    --HG--
    branch : plugins

--- plugins/quick_note/quick_note.cpp
+++ plugins/quick_note/quick_note.cpp
@@ -2,6 +2,8 @@
 #include <QGridLayout>
 #include <QInputDialog>
 #include "plugin_settings.h"
+#include "skin_drawer.h"
+#include "text_skin.h"
 #include "quick_note_settings.h"
 #include "quick_note.h"
 
@@ -9,6 +11,7 @@ namespace quick_note {
 
 QuickNote::QuickNote() {
   settings_ = new PluginSettings("Nick Korotysh", "Digital Clock", this);
+  drawer_ = new skin_draw::SkinDrawer(this);
 
   InitTranslator(QLatin1String(":/quick_note/quick_note_"));
   info_.display_name = tr("Quick note");
@@ -21,11 +24,16 @@ void QuickNote::Init(QWidget* main_wnd) {
   InitDefaults(&defaults);
   settings_->SetDefaultValues(defaults);
   settings_->Load();
+  drawer_->SetSpace(0);
 }
 
 void QuickNote::Start() {
   msg_label_ = new QLabel();
   main_layout_->addWidget(msg_label_, main_layout_->rowCount(), 0, 1, main_layout_->columnCount());
+  connect(drawer_, &skin_draw::SkinDrawer::DrawingFinished, [=] (const QImage& img) {
+    msg_label_->setPixmap(QPixmap::fromImage(img));
+    qobject_cast<QWidget*>(main_layout_->parent())->adjustSize();
+  });
   ApplyString(settings_->GetOption(OPT_QUICK_NOTE_MSG).toString());
 }
 
@@ -60,9 +68,42 @@ void QuickNote::Configure() {
 }
 
 void QuickNote::SettingsListener(Options option, const QVariant& new_value) {
+  switch (option) {
+    case OPT_FONT:
+    {
+      skin_draw::ISkin::SkinPtr skin(new skin_draw::TextSkin(new_value.value<QFont>()));
+      drawer_->ApplySkin(skin);
+      break;
+    }
+
+    case OPT_ZOOM:
+      drawer_->SetZoom(new_value.toReal());
+      break;
+
+    case OPT_COLOR:
+      drawer_->SetColor(new_value.value<QColor>());
+      break;
+
+    case OPT_TEXTURE:
+      drawer_->SetTexture(new_value.toString());
+      break;
+
+    case OPT_TEXTURE_PER_ELEMENT:
+      drawer_->SetTexturePerElement(new_value.toBool());
+      break;
+
+    case OPT_TEXTURE_DRAW_MODE:
+      drawer_->SetTextureDrawMode((skin_draw::SkinDrawer::DrawMode)new_value.toInt());
+      break;
+
+    case OPT_CUSTOMIZATION:
+      drawer_->SetCustomizationType((skin_draw::SkinDrawer::CustomizationType)new_value.toInt());
+      break;
+  }
 }
 
 void QuickNote::ApplyString(const QString& str) {
+  drawer_->SetString(str);
 }
 
 } // namespace quick_note
