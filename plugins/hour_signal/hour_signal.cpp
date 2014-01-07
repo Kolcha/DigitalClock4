da679db0fb3e2a2cc71c5d5254b793be505f2b7d
Nick Korotysh <nick.korotysh@gmail.com>
2014-01-07 12:05:40 +0300

    added icon for hour signal plugin
    --HG--
    branch : plugins

--- plugins/hour_signal/hour_signal.cpp
+++ plugins/hour_signal/hour_signal.cpp
@@ -8,6 +8,7 @@ HourSignal::HourSignal() : last_hour_(-1) {
   InitTranslator(QLatin1String(":/hour_signal/hour_signal_"));
   info_.display_name = tr("Every hour signal");
   info_.description = tr("Plays short signal every hour.");
+  info_.icon.load(":/hour_signal/icon.png");
 }
 
 void HourSignal::TimeUpdateListener() {
