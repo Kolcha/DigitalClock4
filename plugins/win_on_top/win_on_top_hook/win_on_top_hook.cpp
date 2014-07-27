8e6ad4d7700b654e322b438f47d47cba9f4d4aff
Nick Korotysh <nick.korotysh@gmail.com>
2014-07-27 23:50:11 +0300

    renamed win_on_top hook library
    --HG--
    branch : bugfixes

--- plugins/win_on_top/win_on_top_hook/win_on_top_hook.cpp
+++ plugins/win_on_top/win_on_top_hook/win_on_top_hook.cpp
@@ -93,7 +93,7 @@ __declspec(dllexport) void Stop() {
 }
 
 
-BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved) {
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
   switch (ul_reason_for_call) {
     case DLL_PROCESS_ATTACH:
     case DLL_THREAD_ATTACH:
