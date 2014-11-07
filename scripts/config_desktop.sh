c28b504bc67135039c0e747ad713d47647004c29
Nick Korotysh <nick.korotysh@gmail.com>
2014-11-08 00:42:36 +0300

    added some linux specific stuff

---
+++ scripts/config_desktop.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+#
+# File:   config_desktop.sh
+# Author: Nick Korotysh <nick.korotysh@gmail.com>
+#
+# Copyright (C) 2014 Nick Korotysh
+#
+# Created on November 7, 2014, 10:36 PM
+#
+# Script to configure .desktop file on target system
+#
+
+cd ${0%/*}
+
+DESKTOP_FILE="digital_clock.desktop"
+APP_EXEC="$PWD/digital_clock.sh"
+APP_ICON="$PWD/digital_clock.svg"
+
+# change .desktop file
+APP_EXEC_S=$(echo $APP_EXEC | sed 's/\//\\\//g')
+APP_ICON_S=$(echo $APP_ICON | sed 's/\//\\\//g')
+  
+sed -i "s/^Exec=.*$/Exec=$APP_EXEC_S/g" $DESKTOP_FILE
+sed -i "s/^Icon=.*$/Icon=$APP_ICON_S/g" $DESKTOP_FILE
+
+# parse script options
+if [[ "$1" == "--help" ]]
+then
+  echo "Supported options:"
+  echo "  --autostart <arg>  : enable/disable clock autostart"
+  echo "      <arg> values: 'enable'/'disable'"
+  echo ""
+  echo "  --icon-color <arg> : change application icon color"
+  echo "      <arg> value : hex HTML color without '#'"
+  exit
+fi
+
+if [[ "$1" == "--autostart" ]]
+then
+  STARTUP_DIR="$HOME/.config/autostart"
+  case "$2" in
+    "enable")
+      [ -d $STARTUP_DIR ] || mkdir -p $STARTUP_DIR
+      cp $DESKTOP_FILE $STARTUP_DIR/$DESKTOP_FILE
+    ;;
+
+    "disable")
+      rm -f $STARTUP_DIR/$DESKTOP_FILE
+    ;;
+
+    *)
+      echo "ivalid argument"
+    ;;
+  esac
+  exit
+fi
+
+if [[ "$1" == "--icon-color" ]]
+then
+  color=$(echo "$2" | grep -e "[A-Fa-f0-9]\{6\}")
+  if [ -z "$color" ]
+  then
+    echo "invalid argument"
+    exit
+  fi
+  sed -i "s/fill=\"\#[A-Fa-f0-9]\{6\}\"/fill=\"\#$color\"/g" $APP_ICON
+  exit
+fi
+
