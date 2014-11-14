ded1daed453e4a3f530808caaca4f41e7fbc2b84
Nick Korotysh <nick.korotysh@gmail.com>
2014-11-14 22:51:55 +0300

    improved linux packeger script

--- scripts/create_package.sh
+++ scripts/create_package.sh
@@ -5,7 +5,7 @@ QT_PATH="$HOME/Programming/Qt/5.3/gcc_64/bin"
 CLOCK_APP_NAME="Digital Clock 4"
 
 CLOCK_SRC_PATH="$HOME/Documents/Qt/digital_clock_4"
-CLOCK_DATA_PATH="$CLOCK_SRC_PATH/../data"
+CLOCK_DATA_PATH="$HOME/Documents/Qt/digital_clock_data"
 
 cd "$CLOCK_SRC_PATH"
 
@@ -18,7 +18,10 @@ TARGET_APP_FOLDER="$CLOCK_SRC_PATH/../$CLOCK_APP_NAME"
 rm -rf "$TARGET_APP_FOLDER"
 mkdir "$TARGET_APP_FOLDER"
 
-# all app files to target folder
+# copy Qt runtime files
+cp -r "$CLOCK_DATA_PATH/qt_linux/"* "$TARGET_APP_FOLDER/"
+
+# copy all app files to target folder
 cp digital_clock/digital_clock "$TARGET_APP_FOLDER/"
 cp clock_common/libclock_common.so.1.0.3 "$TARGET_APP_FOLDER/libclock_common.so.1"
 cp skin_draw/libskin_draw.so.1.2.2 "$TARGET_APP_FOLDER/libskin_draw.so.1"
@@ -38,8 +41,8 @@ cp digital_clock/resources/digital_clock.desktop "$TARGET_APP_FOLDER/"
 cp digital_clock/resources/images/time-24.svg "$TARGET_APP_FOLDER/digital_clock.svg"
 cp scripts/digital_clock.sh "$TARGET_APP_FOLDER/"
 cp scripts/config_desktop.sh "$TARGET_APP_FOLDER/"
-cp -r $CLOCK_DATA_PATH/skins "$TARGET_APP_FOLDER/"
-cp -r $CLOCK_DATA_PATH/textures "$TARGET_APP_FOLDER/"
+cp -r "$CLOCK_DATA_PATH/skins" "$TARGET_APP_FOLDER/"
+cp -r "$CLOCK_DATA_PATH/textures" "$TARGET_APP_FOLDER/"
 
 # cleanup
 make distclean
