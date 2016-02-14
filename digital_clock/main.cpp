dab54db81e95370a4691bee1477b2c5aceffe4ed
Nick Korotysh <nick.korotysh@gmail.com>
2016-02-14 17:19:41 +0300

    get back translation support
    --HG--
    branch : new_settings

--- digital_clock/main.cpp
+++ digital_clock/main.cpp
@@ -2,6 +2,8 @@
 
 #include <QApplication>
 #include <QIcon>
+#include <QTranslator>
+#include <QLibraryInfo>
 
 int main(int argc, char *argv[])
 {
@@ -14,6 +16,29 @@ int main(int argc, char *argv[])
   app.setWindowIcon(QIcon(":/clock/images/clock.svg"));
   app.setQuitOnLastWindowClosed(false);
   app.setAttribute(Qt::AA_UseHighDpiPixmaps);
+
+  // install app translators
+  QTranslator app_translator;
+  QTranslator qt_translator;
+  QStringList ui_languages = QLocale::system().uiLanguages();
+  foreach (QString locale, ui_languages) {
+    locale = QLocale(locale).name();
+    if (app_translator.load(QLatin1String(":/clock/languages/digital_clock_") + locale)) {
+      if (qt_translator.load(QLatin1String("qt_") + locale,
+                             QLibraryInfo::location(QLibraryInfo::TranslationsPath))) {
+        app.installTranslator(&app_translator);
+        app.installTranslator(&qt_translator);
+        break;
+      }
+      app_translator.load(QString()); // unload()
+    } else if (locale == QLatin1String("C") /* overrideLanguage == "English" */) {
+      // use built-in
+      break;
+    } else if (locale.startsWith(QLatin1String("en")) /* "English" is built-in */) {
+      // use built-in
+      break;
+    }
+  }
 
   digital_clock::MainWindow wnd;
   wnd.show();
