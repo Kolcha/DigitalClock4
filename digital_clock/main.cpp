95c514faed2b8612de1eba977e4e59c20179324b
Nick Korotysh <nick.korotysh@gmail.com>
2016-02-27 06:19:49 +0300

    used russian for ukrainian locale

--- digital_clock/main.cpp
+++ digital_clock/main.cpp
@@ -37,6 +37,13 @@ int main(int argc, char *argv[])
     } else if (locale.startsWith(QLatin1String("en")) /* "English" is built-in */) {
       // use built-in
       break;
+    } else if (locale.contains("ua", Qt::CaseInsensitive)) /* Ukrainian, use russian */ {
+      app_translator.load(QLatin1String(":/clock/languages/digital_clock_ru"));
+      qt_translator.load(QLatin1String("qt_ru"),
+                         QLibraryInfo::location(QLibraryInfo::TranslationsPath));
+      app.installTranslator(&app_translator);
+      app.installTranslator(&qt_translator);
+      break;
     }
   }
 
