9ee4fbb44b66eefd11b35f2f129402e206584e1c
Nick Korotysh <nick.korotysh@gmail.com>
2016-05-24 21:45:07 +0300

    improved linux build script

--- scripts/create_package.sh
+++ scripts/create_package.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-QT_PATH="$HOME/Qt/5.6/gcc_64/bin"
+export QT_ROOT="$HOME/Qt/5.6/gcc_64"
 
 CLOCK_APP_NAME="Digital Clock 4"
 
@@ -9,8 +9,8 @@ CLOCK_DATA_PATH="$HOME/Documents/Qt/digital_clock_data"
 
 # some validation
 failed=0
-[ -d "$QT_PATH" ] || { failed=1; }
-[[ $failed == 1 ]] && echo "QT_PATH=$QT_PATH doesn't exists"
+[ -d "$QT_ROOT" ] || { failed=1; }
+[[ $failed == 1 ]] && echo "QT_ROOT=$QT_ROOT doesn't exists"
 [ -d "$CLOCK_SRC_PATH" ] || { failed=1; }
 [[ $failed == 1 ]] && echo "CLOCK_SRC_PATH=$CLOCK_SRC_PATH doesn't exists"
 [ -d "$CLOCK_DATA_PATH" ] || { failed=1; }
@@ -19,9 +19,15 @@ failed=0
 
 cd "$CLOCK_SRC_PATH"
 
-$QT_PATH/lupdate -no-obsolete DigitalClock.pro
-$QT_PATH/qmake -config release
-make
+$QT_ROOT/bin/lupdate -no-obsolete DigitalClock.pro
+
+build_dir="$CLOCK_SRC_PATH/../build"
+rm -rf "$build_dir"
+mkdir "$build_dir"
+cd "$build_dir"
+
+$QT_ROOT/bin/qmake -config release -r "$CLOCK_SRC_PATH/DigitalClock.pro"
+make -j4
 [[ $? == 0 ]] || exit 1
 
 # create targer folder
@@ -29,9 +35,6 @@ TARGET_APP_FOLDER="$CLOCK_SRC_PATH/../$CLOCK_APP_NAME"
 rm -rf "$TARGET_APP_FOLDER"
 mkdir "$TARGET_APP_FOLDER"
 [[ $? == 0 ]] || exit 1
-
-# copy Qt runtime files
-cp -r "$CLOCK_DATA_PATH/qt_linux/"* "$TARGET_APP_FOLDER/"
 
 # copy all app files to target folder
 cp digital_clock/digital_clock "$TARGET_APP_FOLDER/"
@@ -48,24 +51,47 @@ do
 done
 cd ..
 
+# deploy qt runtime
+cd "$TARGET_APP_FOLDER"
+
+"$CLOCK_SRC_PATH/scripts/linux_deploy_qt.sh" "digital_clock"
+"$CLOCK_SRC_PATH/scripts/linux_deploy_qt.sh" "libclock_common.so.1"
+"$CLOCK_SRC_PATH/scripts/linux_deploy_qt.sh" "libskin_draw.so.1"
+
+for i in $(ls -1 plugins)
+do
+  if [[ -f "plugins/$i" ]]
+  then
+    "$CLOCK_SRC_PATH/scripts/linux_deploy_qt.sh" "plugins/$i"
+  fi
+done
+
+# generate qt.conf
+rm -f qt.conf
+echo "[Paths]"                     >> qt.conf
+echo "Plugins = plugins"           >> qt.conf
+echo "Translations = translations" >> qt.conf
+
 # copy resources and some specific stuff
-cp digital_clock/resources/digital_clock.desktop "$TARGET_APP_FOLDER/"
-cp digital_clock/resources/images/time-24.svg "$TARGET_APP_FOLDER/digital_clock.svg"
-cp scripts/digital_clock.sh "$TARGET_APP_FOLDER/"
-cp scripts/config_desktop.sh "$TARGET_APP_FOLDER/"
+cp "$CLOCK_SRC_PATH/digital_clock/resources/digital_clock.desktop" "$TARGET_APP_FOLDER/"
+cp "$CLOCK_SRC_PATH/digital_clock/resources/images/time-24.svg" "$TARGET_APP_FOLDER/digital_clock.svg"
+cp "$CLOCK_SRC_PATH/scripts/digital_clock.sh" "$TARGET_APP_FOLDER/"
+cp "$CLOCK_SRC_PATH/scripts/config_desktop.sh" "$TARGET_APP_FOLDER/"
 cp -r "$CLOCK_DATA_PATH/skins" "$TARGET_APP_FOLDER/"
 cp -r "$CLOCK_DATA_PATH/textures" "$TARGET_APP_FOLDER/"
 
+cd "$TARGET_APP_FOLDER/.."
+
 # cleanup
-make distclean
+rm -rf "$build_dir"
 
 # create archive
-cd "$TARGET_APP_FOLDER/.."
 rm -f digital_clock_4-linux.tar.xz
 tar cJvf digital_clock_4-linux.tar.xz "$CLOCK_APP_NAME"
 [[ $? == 0 ]] || exit 1
 
 # create source tarball
+cd "$CLOCK_SRC_PATH/.."
 cp -r "$CLOCK_SRC_PATH" digital_clock_4-src
 cd digital_clock_4-src
 hg clean --all
---
+++ scripts/linux_deploy_qt.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# plugins lists
+# core gui widgets
+gui_lst="egldeviceintegrations generic imageformats platforminputcontexts platforms platformthemes xcbglintegrations"
+# network
+net_lst="bearer"
+# multimedia
+mm_lst="audio mediaservice playlistformats"
+# svg
+svg_lst="iconengines"
+
+
+function deploy_plugin() {
+  qt_libs=$(ldd "$QT_ROOT/$1" | grep "$QT_ROOT" | grep -o -e '\/.*\.so\.[0-9]\+')
+  for f in $qt_libs
+  do
+    [[ -f $(basename "$f") ]] || cp -L "$f" ./
+  done
+}
+
+
+function deploy_plugins_list() {
+  for i in $1
+  do
+    [[ -d "plugins/$i" ]] || cp -r "$QT_ROOT/plugins/$i" plugins/
+    for plg in $(ls -1 "plugins/$i/")
+    do
+      deploy_plugin "plugins/$i/$plg"
+    done
+  done
+}
+
+
+[[ -d plugins ]] || mkdir plugins
+
+qt_libs=$(ldd "$1" | grep "$QT_ROOT" | grep -o -e '\/.*\.so\.[0-9]\+')
+for f in $qt_libs
+do
+  [[ -f $(basename "$f") ]] || cp -L "$f" ./
+  [[ $(echo "$f" | grep -i "gui") != "" ]] && deploy_plugins_list "$gui_lst"
+  [[ $(echo "$f" | grep -i "network") != "" ]] && deploy_plugins_list "$net_lst"
+  [[ $(echo "$f" | grep -i "multimedia") != "" ]] && deploy_plugins_list "$mm_lst"
+  [[ $(echo "$f" | grep -i "svg") != "" ]] && deploy_plugins_list "$svg_lst"
+done
