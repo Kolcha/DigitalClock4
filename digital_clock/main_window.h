fc9b622950c22dc9e756b604a14fc59feae79778
Nick Korotysh <nick.korotysh@gmail.com>
2016-09-10 03:29:57 +0300

    code cleanup

--- digital_clock/main_window.h
+++ digital_clock/main_window.h
@@ -29,13 +29,10 @@ class ClockWidget;
 class MainWindow : public QWidget
 {
   Q_OBJECT
+
 public:
   explicit MainWindow(QWidget* parent = 0);
   ~MainWindow();
-
-signals:
-
-public slots:
 
 protected:
   void mousePressEvent(QMouseEvent* event);
