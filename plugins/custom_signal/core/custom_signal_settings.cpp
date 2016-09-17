e4547314e296fa7e107f07211d37ebbf8c3bd34a
Nick Korotysh <nick.korotysh@gmail.com>
2016-09-17 22:02:44 +0300

    custom_signal: changed default settings
    --HG--
    branch : plugins

--- plugins/custom_signal/core/custom_signal_settings.cpp
+++ plugins/custom_signal/core/custom_signal_settings.cpp
@@ -8,17 +8,17 @@ void InitDefaults(QSettings::SettingsMap* defaults)
 {
   defaults->insert(OPT_EVERY_HOUR_ENABLED, true);
   defaults->insert(OPT_EVERY_HOUR_SIGNAL, QUrl("qrc:/custom_signal/eh_signal.wav"));
-  defaults->insert(OPT_EVERY_HOUR_VOLUME, 80);
+  defaults->insert(OPT_EVERY_HOUR_VOLUME, 90);
   defaults->insert(OPT_EVERY_HOUR_LAST_FILE, QString());
 
   defaults->insert(OPT_QUARTER_HOUR_ENABLED, false);
   defaults->insert(OPT_QUARTER_HOUR_SIGNAL, QUrl("qrc:/custom_signal/qh_signal.wav"));
-  defaults->insert(OPT_QUARTER_HOUR_VOLUME, 80);
+  defaults->insert(OPT_QUARTER_HOUR_VOLUME, 60);
   defaults->insert(OPT_QUARTER_HOUR_LAST_FILE, QString());
 
   defaults->insert(OPT_CUSTOM_PERIOD_ENABLED, false);
-  defaults->insert(OPT_CUSTOM_PERIOD_SIGNAL, QUrl("qrc:/custom_signal/eh_signal.wav"));
-  defaults->insert(OPT_CUSTOM_PERIOD_VOLUME, 80);
+  defaults->insert(OPT_CUSTOM_PERIOD_SIGNAL, QUrl("qrc:/custom_signal/bell.wav"));
+  defaults->insert(OPT_CUSTOM_PERIOD_VOLUME, 30);
   defaults->insert(OPT_CUSTOM_PERIOD_PERIOD, 10);
   defaults->insert(OPT_CUSTOM_PERIOD_OFFSET, 0);
   defaults->insert(OPT_CUSTOM_PERIOD_LAST_FILE, QString());
