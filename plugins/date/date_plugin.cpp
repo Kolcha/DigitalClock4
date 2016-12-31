c6808934f239b07da598e441a1060fe06e4cbf65
Nick Korotysh <nick.korotysh@gmail.com>
2017-01-01 00:42:01 +0300

    It's 2017!

--- plugins/date/date_plugin.cpp
+++ plugins/date/date_plugin.cpp
@@ -1,6 +1,6 @@
 /*
     Digital Clock: date plugin
-    Copyright (C) 2014-2016  Nick Korotysh <nick.korotysh@gmail.com>
+    Copyright (C) 2014-2017  Nick Korotysh <nick.korotysh@gmail.com>
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
