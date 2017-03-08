a4d0ccd162db4a44d22454016fbc2e61fdac75f2
Nick Korotysh <nick.korotysh@gmail.com>
2017-03-08 14:22:04 +0300

    typo

--- digital_clock/core/clock_settings.cpp
+++ digital_clock/core/clock_settings.cpp
@@ -152,7 +152,7 @@ QVariant ClockSettings::GetDefaultValue(const int id) const
     // updater settings
     case OPT_USE_AUTOUPDATE:        return true;
     case OPT_UPDATE_PERIOD:         return 3;
-    case OPT_CHECK_FOR_BETA:        return *(QApplication::applicationVersion().rbegin()) == "+";
+    case OPT_CHECK_FOR_BETA:        return *(QApplication::applicationVersion().rbegin()) == '+';
     // misc settings
     case OPT_CLOCK_URL_ENABLED:     return false;
     case OPT_CLOCK_URL_STRING:      return QString("http://digitalclock4.sourceforge.net/");
