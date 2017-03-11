451e4a211bc2b01a40a1ce37e84dab5eb3bd67fc
Nick Korotysh <nick.korotysh@gmail.com>
2017-03-11 13:50:02 +0300

    plugin_core: updated translations
    --HG--
    branch : plugins

--- plugin_core/plugin_core_ru.ts
+++ plugin_core/plugin_core_ru.ts
@@ -4,67 +4,92 @@
 <context>
     <name>plugin::BaseSettingsWidget</name>
     <message>
-        <location filename="base_settings_widget.ui" line="17"/>
+        <location filename="base_settings_widget.ui" line="72"/>
         <source>Font</source>
         <translation>Шрифт</translation>
     </message>
     <message>
-        <location filename="base_settings_widget.ui" line="23"/>
+        <location filename="base_settings_widget.ui" line="78"/>
         <source>use clock font</source>
         <translation>использовать шрифт часов</translation>
     </message>
     <message>
-        <location filename="base_settings_widget.ui" line="35"/>
+        <location filename="base_settings_widget.ui" line="90"/>
         <source>custom</source>
         <translation>пользовательский</translation>
     </message>
     <message>
-        <location filename="base_settings_widget.ui" line="70"/>
-        <source>Zoom</source>
-        <translation>Масштабирование</translation>
+        <location filename="base_settings_widget.ui" line="23"/>
+        <source>leave as is</source>
+        <translation>оставить как есть</translation>
     </message>
     <message>
-        <location filename="base_settings_widget.ui" line="76"/>
-        <source>do not zoom</source>
-        <translation>не масштабировать</translation>
+        <location filename="base_settings_widget.ui" line="17"/>
+        <source>Font size</source>
+        <translation>Размер шрифта</translation>
     </message>
     <message>
-        <location filename="base_settings_widget.ui" line="83"/>
-        <source>calculate font size</source>
-        <translation>рассчитать размер шрифта</translation>
+        <location filename="base_settings_widget.ui" line="30"/>
+        <source>percent of clock size</source>
+        <translation>процент от размера часов</translation>
     </message>
     <message>
-        <location filename="base_settings_widget.ui" line="93"/>
-        <source>use clock zoom</source>
-        <translation>использовать масштаб часов</translation>
-    </message>
-    <message>
-        <location filename="base_settings_widget.ui" line="103"/>
+        <location filename="base_settings_widget.ui" line="125"/>
         <source>Alignment</source>
         <translation>Выравнивание</translation>
     </message>
     <message>
-        <location filename="base_settings_widget.ui" line="109"/>
+        <location filename="base_settings_widget.ui" line="131"/>
+        <source>top-left</source>
+        <translation>верхний левый</translation>
+    </message>
+    <message>
+        <location filename="base_settings_widget.ui" line="138"/>
+        <source>top</source>
+        <translation>по верху</translation>
+    </message>
+    <message>
+        <location filename="base_settings_widget.ui" line="145"/>
+        <source>top-right</source>
+        <translation>верхний правый</translation>
+    </message>
+    <message>
+        <location filename="base_settings_widget.ui" line="152"/>
         <source>left</source>
         <translation>по левому краю</translation>
     </message>
     <message>
-        <location filename="base_settings_widget.ui" line="116"/>
+        <location filename="base_settings_widget.ui" line="159"/>
         <source>center</source>
         <translation>по центру</translation>
     </message>
     <message>
-        <location filename="base_settings_widget.ui" line="126"/>
+        <location filename="base_settings_widget.ui" line="169"/>
         <source>right</source>
         <translation>по правому краю</translation>
     </message>
     <message>
-        <location filename="base_settings_widget.ui" line="136"/>
+        <location filename="base_settings_widget.ui" line="176"/>
+        <source>bottom-left</source>
+        <translation>нижний левый</translation>
+    </message>
+    <message>
+        <location filename="base_settings_widget.ui" line="183"/>
+        <source>bottom</source>
+        <translation>по низу</translation>
+    </message>
+    <message>
+        <location filename="base_settings_widget.ui" line="190"/>
+        <source>bottom-right</source>
+        <translation>нижний правый</translation>
+    </message>
+    <message>
+        <location filename="base_settings_widget.ui" line="200"/>
         <source>Color</source>
         <translation>Цвет</translation>
     </message>
     <message>
-        <location filename="base_settings_widget.ui" line="142"/>
+        <location filename="base_settings_widget.ui" line="206"/>
         <source>use custom color</source>
         <translation>использовать пользовательский цвет</translation>
     </message>
