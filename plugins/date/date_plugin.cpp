f9d4509f126ca6e5a2331cd620bbda12c3df3505
Nick Korotysh <nick.korotysh@gmail.com>
2017-03-12 12:31:36 +0300

    updated plugins to use new options
    --HG--
    branch : plugins

--- plugins/date/date_plugin.cpp
+++ plugins/date/date_plugin.cpp
@@ -19,7 +19,6 @@
 #include "date_plugin.h"
 
 #include <QLabel>
-#include <QGridLayout>
 #include <QVBoxLayout>
 #include <QDateTime>
 
@@ -92,7 +91,7 @@ void DatePlugin::InitSettingsDefaults(QSettings::SettingsMap* defaults)
 QWidget* DatePlugin::InitWidget(QGridLayout* layout)
 {
   msg_label_ = new QLabel();
-  layout->addWidget(msg_label_, layout->rowCount(), 0, 1, layout->columnCount());
+  Q_UNUSED(layout);
   return msg_label_;
 }
 
