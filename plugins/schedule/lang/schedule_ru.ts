26b7b3e21b124c4cd841d64d0a2683c583d74084
Nick Korotysh <nick.korotysh@gmail.com>
2017-08-22 22:06:32 +0300

    schedule: updated resources
    --HG--
    branch : plugins

--- plugins/schedule/lang/schedule_ru.ts
+++ plugins/schedule/lang/schedule_ru.ts
@@ -4,17 +4,19 @@
 <context>
     <name>schedule::Schedule</name>
     <message>
-        <location filename="../schedule.cpp" line="33"/>
+        <location filename="../schedule.cpp" line="35"/>
         <source>Scheduler</source>
         <translation>Планировщик</translation>
     </message>
     <message>
-        <location filename="../schedule.cpp" line="34"/>
+        <location filename="../schedule.cpp" line="36"/>
         <source>Allow to schedule a task and display notification at specified time.</source>
         <translation>Позволяет запланировать задание и отображает уведомление в указанное время.</translation>
     </message>
     <message>
-        <location filename="../schedule.cpp" line="108"/>
+        <location filename="../schedule.cpp" line="113"/>
+        <location filename="../schedule.cpp" line="120"/>
+        <location filename="../schedule.cpp" line="126"/>
         <source>Scheduled task</source>
         <translation>Запланированое задание</translation>
     </message>
@@ -32,12 +34,12 @@
         <translation>Добавить</translation>
     </message>
     <message>
-        <location filename="../gui/schedule_dialog.ui" line="30"/>
+        <location filename="../gui/schedule_dialog.ui" line="33"/>
         <source>Edit</source>
         <translation>Править</translation>
     </message>
     <message>
-        <location filename="../gui/schedule_dialog.ui" line="37"/>
+        <location filename="../gui/schedule_dialog.ui" line="43"/>
         <source>Delete</source>
         <translation>Удалить</translation>
     </message>
@@ -48,6 +50,38 @@
         <location filename="../gui/task_edit_dialog.ui" line="14"/>
         <source>Task Editing</source>
         <translation>Редактирование задания</translation>
+    </message>
+    <message>
+        <location filename="../gui/task_edit_dialog.ui" line="54"/>
+        <source>Notification</source>
+        <translation>Уведомление</translation>
+    </message>
+    <message>
+        <location filename="../gui/task_edit_dialog.ui" line="60"/>
+        <source>&amp;Balloon message</source>
+        <translation>&amp;Всплывающее сообщение</translation>
+    </message>
+    <message>
+        <location filename="../gui/task_edit_dialog.ui" line="70"/>
+        <source>&amp;Timeout:</source>
+        <translation>&amp;Таймаут:</translation>
+    </message>
+    <message>
+        <location filename="../gui/task_edit_dialog.ui" line="86"/>
+        <source>Message &amp;dialog</source>
+        <translation>&amp;Диалоговое окно</translation>
+    </message>
+    <message>
+        <location filename="../gui/task_edit_dialog.ui" line="106"/>
+        <source>Preview</source>
+        <translation>Просмотр</translation>
+    </message>
+    <message>
+        <location filename="../gui/task_edit_dialog.cpp" line="115"/>
+        <location filename="../gui/task_edit_dialog.cpp" line="120"/>
+        <location filename="../gui/task_edit_dialog.cpp" line="126"/>
+        <source>Task preview</source>
+        <translation>Предварительный просмотр</translation>
     </message>
 </context>
 </TS>
