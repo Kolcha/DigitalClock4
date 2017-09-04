8126aa44df79c83b7fd0701fdcd600c7125cc816
Nick Korotysh <nick.korotysh@gmail.com>
2017-09-04 14:31:37 +0300

    a lot of cosmetic changes
    --HG--
    branch : plugins

--- plugins/countdown_timer/lang/countdown_timer_ru.ts
+++ plugins/countdown_timer/lang/countdown_timer_ru.ts
@@ -2,54 +2,64 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="ru_RU">
 <context>
-    <name>date::DatePlugin</name>
+    <name>countdown_timer::CountdownTimerPlugin</name>
     <message>
-        <location filename="../date_plugin.cpp" line="35"/>
-        <source>Date</source>
-        <translation>Дата</translation>
+        <location filename="../countdown_timer_plugin.cpp" line="37"/>
+        <source>Countdown timer</source>
+        <translation>Таймер обратного отсчёта</translation>
     </message>
     <message>
-        <location filename="../date_plugin.cpp" line="36"/>
-        <source>Allows to display current date under clock.</source>
-        <translation>Отображает текущую дату под часами.</translation>
+        <location filename="../countdown_timer_plugin.cpp" line="38"/>
+        <source>Just a countdown timer.</source>
+        <translation>Простой таймер обратного отсчёта.</translation>
     </message>
 </context>
 <context>
-    <name>date::SettingsDialog</name>
+    <name>countdown_timer::SettingsDialog</name>
     <message>
         <location filename="../gui/settings_dialog.ui" line="14"/>
-        <source>Date plugin settings</source>
-        <translation>Настройки плагина &quot;Дата&quot;</translation>
+        <source>Countdown timer plugin settings</source>
+        <translation>Насройки плагина &quot;Таймер обратного отсчёта&quot;</translation>
     </message>
     <message>
-        <location filename="../gui/settings_dialog.ui" line="26"/>
-        <source>Format</source>
-        <translation>Формат</translation>
+        <location filename="../gui/settings_dialog.ui" line="27"/>
+        <source>Timer</source>
+        <translation>Таймер</translation>
     </message>
     <message>
-        <location filename="../gui/settings_dialog.ui" line="32"/>
-        <source>system</source>
-        <translation>системный</translation>
+        <location filename="../gui/settings_dialog.ui" line="42"/>
+        <source>target time</source>
+        <translation>конкретное время</translation>
     </message>
     <message>
-        <location filename="../gui/settings_dialog.ui" line="45"/>
-        <source>custom</source>
-        <translation>пользовательский</translation>
+        <location filename="../gui/settings_dialog.ui" line="56"/>
+        <source>interval</source>
+        <translation>интервал</translation>
     </message>
     <message>
-        <location filename="../gui/settings_dialog.cpp" line="33"/>
-        <source>default</source>
-        <translation>по умолчанию</translation>
+        <location filename="../gui/settings_dialog.ui" line="66"/>
+        <source>hours</source>
+        <translation>часы</translation>
     </message>
     <message>
-        <location filename="../gui/settings_dialog.cpp" line="35"/>
-        <source>short</source>
-        <translation>короткий</translation>
+        <location filename="../gui/settings_dialog.ui" line="73"/>
+        <source>minutes</source>
+        <translation>минуты</translation>
     </message>
     <message>
-        <location filename="../gui/settings_dialog.cpp" line="36"/>
-        <source>long</source>
-        <translation>длинный</translation>
+        <location filename="../gui/settings_dialog.ui" line="80"/>
+        <source>seconds</source>
+        <translation>секунды</translation>
+    </message>
+    <message>
+        <location filename="../gui/settings_dialog.ui" line="90"/>
+        <source>show custom message on timeout</source>
+        <translation>показывать сообщение по истечении времени</translation>
+    </message>
+    <message>
+        <location filename="../gui/settings_dialog.cpp" line="42"/>
+        <source>Appearance</source>
+        <translation>Вид</translation>
     </message>
 </context>
 </TS>
