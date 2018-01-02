7e61685d0609781935b4fae8000b709d88161aab
Nick Korotysh <nick.korotysh@gmail.com>
2018-01-02 20:53:23 +0300

    reverted some changes in build configs/scripts

--- scripts/create_package.sh
+++ scripts/create_package.sh
@@ -73,8 +73,8 @@ done
 cp "$build_dir/paletteicon/libpaletteicon.so" "$TARGET_APP_FOLDER/plugins/iconengines/"
 
 # copy SSL libs
-cp "/usr/lib64/libssl.so" "$TARGET_APP_FOLDER/"
-cp "/usr/lib64/libcrypto.so" "$TARGET_APP_FOLDER/"
+cp "/lib/x86_64-linux-gnu/libssl.so.1.0.0" "$TARGET_APP_FOLDER/"
+cp "/lib/x86_64-linux-gnu/libcrypto.so.1.0.0" "$TARGET_APP_FOLDER/"
 
 # generate qt.conf
 rm -f qt.conf
