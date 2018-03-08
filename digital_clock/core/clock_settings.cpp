3f71916f6be4cd39f400ff92e5dc4926b82e57e9
Nick Korotysh <nick.korotysh@gmail.com>
2018-03-08 22:19:00 +0300

    don't display "Show/Hide" menu item by default

--- digital_clock/core/clock_settings.cpp
+++ digital_clock/core/clock_settings.cpp
@@ -171,7 +171,7 @@ QVariant ClockSettings::GetDefaultValue(const int id) const
     case OPT_CLOCK_URL_STRING:      return QString("http://digitalclock4.sourceforge.net/");
     case OPT_FULLSCREEN_IGNORE_LST: return QStringList() << "Chrome_WidgetWin_0" << "CEF-OSR-WIDGET";
     case OPT_BETTER_STAY_ON_TOP:    return false;
-    case OPT_SHOW_HIDE_ENABLED:     return true;
+    case OPT_SHOW_HIDE_ENABLED:     return false;
     case OPT_EXPORT_STATE:          return true;
     case OPT_KEEP_ALWAYS_VISIBLE:   return false;
     case OPT_ONLY_ONE_INSTANCE:     return false;
