d11d6579731ec014308d291e431566a3e6b7b717
Nick Korotysh <nick.korotysh@gmail.com>
2018-04-09 17:14:26 +0300

    linux: display clock on current workspace instead of first

--- digital_clock/platform/main_window_linux.cpp
+++ digital_clock/platform/main_window_linux.cpp
@@ -26,10 +26,37 @@
 namespace digital_clock {
 namespace gui {
 
+static long GetCurrentDesktop(Display* display)
+{
+  Atom actual_type_return;
+  int actual_format_return = 0;
+  unsigned long nitems_return = 0;
+  unsigned long bytes_after_return = 0;
+  unsigned long* desktop = 0;
+  unsigned long ret;
+
+  if (XGetWindowProperty(display, DefaultRootWindow(display),
+                         XInternAtom(display, "_NET_CURRENT_DESKTOP", False),
+                         0, 1, False, XA_CARDINAL,
+                         &actual_type_return, &actual_format_return,
+                         &nitems_return, &bytes_after_return,
+                         reinterpret_cast<unsigned char**>(&desktop)) != Success) {
+    return 0;
+  }
+  if (actual_type_return != XA_CARDINAL || nitems_return == 0) {
+    return 0;
+  }
+
+  ret = desktop[0];
+  XFree(desktop);
+  return ret;
+}
+
 void ClockWindow::SetVisibleOnAllDesktops(bool set)
 {
+  unsigned long desktop = GetCurrentDesktop(QX11Info::display());
   // http://stackoverflow.com/questions/16775352/keep-a-application-window-always-on-current-desktop-on-linux-and-mac/
-  unsigned int data = set ? 0xFFFFFFFF : 0x00000000;
+  unsigned long data = set ? 0xFFFFFFFF : desktop;
   XChangeProperty(QX11Info::display(),
                   winId(),
                   XInternAtom(QX11Info::display(), "_NET_WM_DESKTOP", False),
