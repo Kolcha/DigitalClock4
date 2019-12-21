2f8a91fa05095d5a8b2c3516cb6b7d576fb35679
Nick Korotysh <nick.korotysh@gmail.com>
2019-12-22 00:26:30 +0300

    fixed build with macOS SDK 10.15

--- digital_clock/platform/main_window_mac.cpp
+++ digital_clock/platform/main_window_mac.cpp
@@ -23,16 +23,24 @@
 namespace digital_clock {
 namespace gui {
 
+#if __MAC_OS_X_VERSION_MAX_ALLOWED >= 101500
+#define MSG_SEND_CAST(T) (*(T)&objc_msgSend)
+#else
+#define MSG_SEND_CAST(T) objc_msgSend
+#endif
+
 void ClockWindow::SetVisibleOnAllDesktops(bool set)
 {
+  typedef objc_object*(*ps1)(objc_object*, SEL);
+  typedef void(*ps2)(objc_object*, SEL, int);
   // http://stackoverflow.com/questions/16775352/keep-a-application-window-always-on-current-desktop-on-linux-and-mac/
   WId windowObject = this->winId();
   objc_object* nsviewObject = reinterpret_cast<objc_object*>(windowObject);
-  objc_object* nsWindowObject = objc_msgSend(nsviewObject, sel_registerName("window"));
+  objc_object* nsWindowObject = MSG_SEND_CAST(ps1)(nsviewObject, sel_registerName("window"));
   int NSWindowCollectionBehaviorCanJoinAllSpaces = set ? 1 : 0;
   int NSWindowCollectionBehaviorFullScreenNone = 1 << 9;
   int collectionBehavior = NSWindowCollectionBehaviorCanJoinAllSpaces | NSWindowCollectionBehaviorFullScreenNone;
-  objc_msgSend(nsWindowObject, sel_registerName("setCollectionBehavior:"), collectionBehavior);
+  MSG_SEND_CAST(ps2)(nsWindowObject, sel_registerName("setCollectionBehavior:"), collectionBehavior);
 }
 
 } // namespace gui
