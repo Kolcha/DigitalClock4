d47bc8edcb42aff708cdb6098e46b1e43377ea4a
Nick Korotysh <nick.korotysh@gmail.com>
2020-02-18 22:19:35 +0300

    ported helper script to python3, fixed code style

--- scripts/inc_build.py
+++ scripts/inc_build.py
@@ -1,24 +1,25 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
 
 import os
 import re
 import sys
 
+
 def inc_build(rc_file):
-    f = open(rc_file, "r")
+    f = open(rc_file, "rb")
     data = f.read()
     f.close()
 
     def __inc_build(m):
-        return re.sub(r'\d+$', str(int(m.group(4)) + 1), m.group(0))
+        return re.sub(rb'\d+$', str(int(m[4]) + 1).encode('ascii'), m[0])
 
-    data = re.sub(r'FILEVERSION\s+(\d+),(\d+),(\d+),(\d+)', __inc_build, data)
-    data = re.sub(r'(\d+)\.(\d+)\.(\d+)\.(\d+)', __inc_build, data)
+    data = re.sub(rb'FILEVERSION\s+(\d+),(\d+),(\d+),(\d+)', __inc_build, data)
+    data = re.sub(rb'(\d+)\.(\d+)\.(\d+)\.(\d+)', __inc_build, data)
 
-    f = open(rc_file, "w")
+    f = open(rc_file, "wb")
     f.write(data)
     f.close()
+
 
 if (len(sys.argv) > 1):
     for arg in sys.argv[1:]:
@@ -27,4 +28,5 @@ else:
     from glob import glob
     pwd = os.getcwd()
     lst = [y for x in os.walk(pwd) for y in glob(os.path.join(x[0], '*.rc'))]
-    for arg in lst: inc_build(arg)
+    for arg in lst:
+        inc_build(arg)
