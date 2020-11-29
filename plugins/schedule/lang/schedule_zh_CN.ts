75dcb7fcddcc78279b8b257d84e218214c082ec0
Nick Korotysh <nick.korotysh@gmail.com>
2020-11-29 13:09:09 +0300

    schedule: update translation files
    --HG--
    branch : plugins

--- plugins/schedule/lang/schedule_zh_CN.ts
+++ plugins/schedule/lang/schedule_zh_CN.ts
@@ -2,21 +2,115 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="zh_CN">
 <context>
+    <name>QHotkey</name>
+    <message>
+        <location filename="../../../../3rdparty/QHotkey/QHotkey/qhotkey.cpp" line="277"/>
+        <source>Failed to register %1. Error: %2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../../3rdparty/QHotkey/QHotkey/qhotkey.cpp" line="297"/>
+        <source>Failed to unregister %1. Error: %2</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>schedule::AdvancedSettingsDialog</name>
+    <message>
+        <location filename="../gui/advanced_settings_dialog.ui" line="14"/>
+        <source>Notification Settings</source>
+        <translation>通知设置</translation>
+    </message>
+    <message>
+        <location filename="../gui/advanced_settings_dialog.ui" line="20"/>
+        <source>Preview</source>
+        <translation>预览</translation>
+    </message>
+    <message>
+        <location filename="../gui/advanced_settings_dialog.ui" line="27"/>
+        <source> sec</source>
+        <extracomment>seconds</extracomment>
+        <translation>秒</translation>
+    </message>
+    <message>
+        <location filename="../gui/advanced_settings_dialog.ui" line="34"/>
+        <source>Show additional tray icon for plugin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gui/advanced_settings_dialog.ui" line="51"/>
+        <source>Browse</source>
+        <translation>浏览</translation>
+    </message>
+    <message>
+        <location filename="../gui/advanced_settings_dialog.ui" line="58"/>
+        <source>&amp;Balloon message</source>
+        <translation>&amp;气泡消息</translation>
+    </message>
+    <message>
+        <location filename="../gui/advanced_settings_dialog.ui" line="81"/>
+        <source>Message &amp;dialog</source>
+        <translation>消息 &amp;对话框</translation>
+    </message>
+    <message>
+        <location filename="../gui/advanced_settings_dialog.ui" line="88"/>
+        <source>&amp;Play sound</source>
+        <translation>&amp;播放音效</translation>
+    </message>
+    <message>
+        <location filename="../gui/advanced_settings_dialog.ui" line="95"/>
+        <source>Do not show notification</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gui/advanced_settings_dialog.ui" line="102"/>
+        <source>&amp;Timeout:</source>
+        <translation>&amp;超时：</translation>
+    </message>
+    <message>
+        <location filename="../gui/advanced_settings_dialog.ui" line="125"/>
+        <source>Shortcut for quick task addition</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gui/advanced_settings_dialog.cpp" line="61"/>
+        <source>This is sample task text. Just to test how notification looks.</source>
+        <translation>这是一个任务文本的示例，用以展示通知消息的外观。</translation>
+    </message>
+    <message>
+        <location filename="../gui/advanced_settings_dialog.cpp" line="74"/>
+        <location filename="../gui/advanced_settings_dialog.cpp" line="79"/>
+        <location filename="../gui/advanced_settings_dialog.cpp" line="85"/>
+        <source>Task preview</source>
+        <translation>任务预览</translation>
+    </message>
+    <message>
+        <location filename="../gui/advanced_settings_dialog.cpp" line="118"/>
+        <source>Select sound</source>
+        <translation>选择铃声</translation>
+    </message>
+    <message>
+        <location filename="../gui/advanced_settings_dialog.cpp" line="119"/>
+        <source>Sounds (*.wav *.mp3 *.ogg *.oga *.m4a);;All files (*.*)</source>
+        <translation>声音文件 (*.wav *.mp3 *.ogg *.oga *.m4a);;所有文件 (*.*)</translation>
+    </message>
+</context>
+<context>
     <name>schedule::Schedule</name>
     <message>
-        <location filename="../schedule.cpp" line="38"/>
+        <location filename="../schedule.cpp" line="48"/>
         <source>Scheduler</source>
         <translation>日程表</translation>
     </message>
     <message>
-        <location filename="../schedule.cpp" line="39"/>
+        <location filename="../schedule.cpp" line="49"/>
         <source>Allow to schedule a task and display notification at specified time.</source>
         <translation>允许用户计划日程任务，并在设定时间点显示提醒消息。</translation>
     </message>
     <message>
-        <location filename="../schedule.cpp" line="128"/>
-        <location filename="../schedule.cpp" line="135"/>
-        <location filename="../schedule.cpp" line="141"/>
+        <location filename="../schedule.cpp" line="157"/>
+        <location filename="../schedule.cpp" line="164"/>
+        <location filename="../schedule.cpp" line="170"/>
         <source>Scheduled task</source>
         <translation>计划任务</translation>
     </message>
@@ -27,21 +121,6 @@
         <location filename="../gui/schedule_dialog.ui" line="14"/>
         <source>Schedule</source>
         <translation>计划</translation>
-    </message>
-    <message>
-        <location filename="../gui/schedule_dialog.ui" line="23"/>
-        <source>Add</source>
-        <translation>添加</translation>
-    </message>
-    <message>
-        <location filename="../gui/schedule_dialog.ui" line="33"/>
-        <source>Edit</source>
-        <translation>编辑</translation>
-    </message>
-    <message>
-        <location filename="../gui/schedule_dialog.ui" line="43"/>
-        <source>Delete</source>
-        <translation>删除</translation>
     </message>
 </context>
 <context>
@@ -57,55 +136,60 @@
         <translation>&amp;超时：</translation>
     </message>
     <message>
-        <location filename="../gui/task_advanced_settings_dialog.ui" line="43"/>
+        <location filename="../gui/task_advanced_settings_dialog.ui" line="33"/>
         <source>Message &amp;dialog</source>
         <translation>消息 &amp;对话框</translation>
     </message>
     <message>
-        <location filename="../gui/task_advanced_settings_dialog.ui" line="63"/>
+        <location filename="../gui/task_advanced_settings_dialog.ui" line="40"/>
         <source>Preview</source>
         <translation>预览</translation>
     </message>
     <message>
-        <location filename="../gui/task_advanced_settings_dialog.ui" line="70"/>
+        <location filename="../gui/task_advanced_settings_dialog.ui" line="68"/>
         <source>&amp;Balloon message</source>
         <translation>&amp;气泡消息</translation>
     </message>
     <message>
-        <location filename="../gui/task_advanced_settings_dialog.ui" line="80"/>
+        <location filename="../gui/task_advanced_settings_dialog.ui" line="78"/>
+        <source>Do not show notification</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gui/task_advanced_settings_dialog.ui" line="61"/>
         <source> sec</source>
         <extracomment>seconds</extracomment>
         <translation>秒</translation>
     </message>
     <message>
-        <location filename="../gui/task_advanced_settings_dialog.ui" line="87"/>
+        <location filename="../gui/task_advanced_settings_dialog.ui" line="54"/>
         <source>&amp;Play sound</source>
         <translation>&amp;播放音效</translation>
     </message>
     <message>
-        <location filename="../gui/task_advanced_settings_dialog.ui" line="94"/>
+        <location filename="../gui/task_advanced_settings_dialog.ui" line="47"/>
         <source>Browse</source>
         <translation>浏览</translation>
     </message>
     <message>
-        <location filename="../gui/task_advanced_settings_dialog.cpp" line="64"/>
+        <location filename="../gui/task_advanced_settings_dialog.cpp" line="66"/>
         <source>This is sample task text. Just to test how notification looks.</source>
         <translation>这是一个任务文本的示例，用以展示通知消息的外观。</translation>
     </message>
     <message>
-        <location filename="../gui/task_advanced_settings_dialog.cpp" line="77"/>
-        <location filename="../gui/task_advanced_settings_dialog.cpp" line="82"/>
-        <location filename="../gui/task_advanced_settings_dialog.cpp" line="88"/>
+        <location filename="../gui/task_advanced_settings_dialog.cpp" line="79"/>
+        <location filename="../gui/task_advanced_settings_dialog.cpp" line="84"/>
+        <location filename="../gui/task_advanced_settings_dialog.cpp" line="90"/>
         <source>Task preview</source>
         <translation>任务预览</translation>
     </message>
     <message>
-        <location filename="../gui/task_advanced_settings_dialog.cpp" line="96"/>
+        <location filename="../gui/task_advanced_settings_dialog.cpp" line="98"/>
         <source>Select sound</source>
         <translation>选择铃声</translation>
     </message>
     <message>
-        <location filename="../gui/task_advanced_settings_dialog.cpp" line="97"/>
+        <location filename="../gui/task_advanced_settings_dialog.cpp" line="99"/>
         <source>Sounds (*.wav *.mp3 *.ogg *.oga *.m4a);;All files (*.*)</source>
         <translation>声音文件 (*.wav *.mp3 *.ogg *.oga *.m4a);;所有文件 (*.*)</translation>
     </message>
